f4abecef2d2591e686158bad05e9e2f6eeddbb69
ManifoldFR <wilson.jallet@polytechnique.org>
2022-04-25 16:09:52 +0200

    specify fmtlib version again

---
+++ unittest/node.cpp
@@ -0,0 +1,18 @@
+#include "proxddp/node.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(node)
+
+using namespace proxddp;
+
+using Scalar = double;
+
+BOOST_AUTO_TEST_CASE(test_node1)
+{
+  using Node = NodeTpl<Scalar>;
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
