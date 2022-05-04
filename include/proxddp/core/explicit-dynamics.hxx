884528c8e5523a738b933ae211d78b292de41076
ManifoldFR <wilson.jallet@polytechnique.org>
2022-05-04 15:49:01 +0200

    explicit-dynamics: fix wrong computations of Jacobian

--- include/proxddp/core/explicit-dynamics.hxx
+++ include/proxddp/core/explicit-dynamics.hxx
@@ -16,7 +16,7 @@ namespace proxddp
     // value to the difference between y and the xout_.
     auto d = static_cast<SpecificData&>(data);
     this->forward(x, u, d.xout_);
-    out_space_.difference(y, d.xout_, d.value_);
+    out_space_.difference(y, d.xout_, d.value_);  // xnext - y
   }
 
   template<typename Scalar>
@@ -27,14 +27,15 @@ namespace proxddp
                    Data& data) const
   {
     SpecificData& d = static_cast<SpecificData&>(data);
+    this->forward(x, u, d.xout_);
     this->dForward(x, u, d.Jx_, d.Ju_); // dxnext_(x,u)
     // compose by jacobians of log (xout - y)
     MatrixXs Jtemp(this->ndx2, this->ndx2);
-    out_space_.Jdifference(y, d.xout_, Jtemp, 0);
-    d.Jx_ = Jtemp * d.Jx_;
-    out_space_.Jdifference(y, d.xout_, Jtemp, 1);
-    d.Ju_ = Jtemp * d.Ju_;
-    d.Jy_ = -MatrixXs::Identity(this->ndx2, this->ndx2);
+    out_space_.Jdifference(y, d.xout_, d.Jy_, 0);  // d(xnext - y) / y
+    Jtemp.setZero();
+    out_space_.Jdifference(y, d.xout_, Jtemp, 1);  // d(xnext - y) / xnext
+    d.Jx_ = (Jtemp * d.Jx_).eval();  // chain rule d(log)/dxnext * dxnext_dx
+    d.Ju_ = (Jtemp * d.Ju_).eval();
   }
   
 } // namespace proxddp
