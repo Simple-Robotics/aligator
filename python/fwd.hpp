cc1a3c574b6f6fa647575dcb8bc75c39350bdf35
ManifoldFR <wilson.jallet@polytechnique.org>
2022-05-05 19:40:30 +0200

    python: introduce visitors

--- python/fwd.hpp
+++ python/fwd.hpp
@@ -10,12 +10,16 @@ namespace proxddp
 #include "proxddp/python/context.hpp"
 #include "proxddp/python/macros.hpp"
 
+#include <pinocchio/fwd.hpp>
+#include <pinocchio/bindings/python/utils/std-vector.hpp>
+
 #include <eigenpy/eigenpy.hpp>
 
 namespace proxddp
 {
   namespace python
   {
+    namespace pinpy = pinocchio::python;
     namespace bp = boost::python;
     
     /// Expose ternary functions
@@ -27,6 +31,27 @@ namespace proxddp
     void exposeDynamics();
     void exposeIntegrators();
     void exposeSolvers();
+
+
+    template<typename T>
+    struct ClonePythonVisitor : bp::def_visitor<ClonePythonVisitor<T>>
+    {
+      template<typename PyT>
+      void visit(PyT& obj) const
+      {
+        obj.def("clone", &T::clone, "Clone the object.");
+      }
+    };
+
+    template<typename T>
+    struct CreateDataPythonVisitor : bp::def_visitor<CreateDataPythonVisitor<T>>
+    {
+      template<typename Pyclass>
+      void visit(Pyclass& obj) const
+      {
+        obj.def("createData", &T::createData, "Create a data object.");
+      }
+    };
 
   } // namespace python
 } // namespace proxddp
