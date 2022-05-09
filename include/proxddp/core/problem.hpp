5ca0ee314eabedbd1a9c8edb113c9304c8e19b1b
ManifoldFR <wilson.jallet@polytechnique.org>
2022-05-09 15:08:21 +0200

    problem: store StageModel by value, take in addStage as reference
    * define numSteps(), evaluate(), computeDerivatives(), createData() member funcs
    * define ProblemDataTpl struct

--- include/proxddp/core/problem.hpp
+++ include/proxddp/core/problem.hpp
@@ -3,6 +3,8 @@
 #include "proxddp/fwd.hpp"
 #include "proxddp/core/stage-model.hpp"
 
+#include <fmt/core.h>
+
 
 namespace proxddp
 {
@@ -14,29 +16,87 @@ namespace proxddp
     using StageModel = StageModelTpl<Scalar>;
     using StageData = StageDataTpl<Scalar>;
 
-    /// Stages of the control problem.
-    std::vector<shared_ptr<StageModel>> stages_;
+    using ProblemData = ProblemDataTpl<Scalar>;
 
-    inline std::size_t numStages() const { return stages_.size(); }
+    PROXNLP_DYNAMIC_TYPEDEFS(Scalar);
+
+    /// Stages of the control problem.
+    std::vector<StageModel> stages_;
 
     ShootingProblemTpl() = default;
+    ShootingProblemTpl(const std::vector<StageModel>& stages) : stages_(stages) {}
 
     /// @brief Add a stage to the control problem.
-    void addStage(const shared_ptr<StageModel>& new_stage);
-    /// @copybrief addStage()  
-    void addStage(shared_ptr<StageModel>&& new_stage);
+    void addStage(const StageModel& new_stage);
+    /// @copybrief addStage()
+    void addStage(StageModel&& new_stage);
 
-    std::vector<shared_ptr<StageData>> createData() const
+    inline std::size_t numSteps() const;
+
+    /// @brief Rollout the problem costs, constraints, dynamics, stage per stage.
+    void evaluate(const std::vector<VectorXs>& xs,
+                  const std::vector<VectorXs>& us,
+                  ProblemData& prob_data) const
     {
-      std::vector<shared_ptr<StageData>> data_vec;
-      data_vec.reserve(stages_.size());
-      for (std::size_t i = 0; i < stages_.size(); i++)
+      const std::size_t nsteps = numSteps();
+      const bool sizes_correct = (xs.size() == nsteps + 1) && (us.size() == nsteps);
+      if (not sizes_correct)
       {
-        data_vec.push_back(std::move(stages_[i]->createData()));
+        throw std::runtime_error(
+          fmt::format("Wrong size for xs or us, expected us.size = {:d}", nsteps));
       }
-      return data_vec;
+
+      for (std::size_t i = 0; i < nsteps; i++)
+      {
+        const StageModel& stage = stages_[i];
+        stage.evaluate(xs[i], us[i], xs[i + 1], *prob_data.stage_data[i]);
+      }
     }
 
+    /// @brief Rollout the problem derivatives, stage per stage.
+    void computeDerivatives(const std::vector<VectorXs>& xs,
+                            const std::vector<VectorXs>& us,
+                            ProblemData& prob_data) const
+    {
+      const std::size_t nsteps = numSteps();
+      const bool sizes_correct = (xs.size() == nsteps + 1) && (us.size() == nsteps);
+      if (not sizes_correct)
+      {
+        throw std::runtime_error(
+          fmt::format("Wrong size for xs or us, expected us.size = {:d}", nsteps));
+      }
+
+      for (std::size_t i = 0; i < nsteps; i++)
+      {
+        const StageModel& stage = stages_[i];
+        stage.computeDerivatives(xs[i], us[i], xs[i + 1], *prob_data.stage_data[i]);
+      }
+    }
+
+    shared_ptr<ProblemData> createData() const
+    {
+      return std::make_shared<ProblemData>(*this);
+    }
+
+  };
+
+  template<typename _Scalar>
+  struct ProblemDataTpl
+  {
+    using Scalar = _Scalar;
+    PROXNLP_DYNAMIC_TYPEDEFS(Scalar);
+
+    using StageDataPtr = shared_ptr<StageDataTpl<Scalar>>;
+    std::vector<StageDataPtr> stage_data;
+
+    ProblemDataTpl(const ShootingProblemTpl<Scalar>& problem)
+    {
+      stage_data.reserve(problem.numSteps());
+      for (std::size_t i = 0; i < problem.numSteps(); i++)
+      {
+        stage_data.push_back(std::move(problem.stages_[i].createData()));
+      }
+    }
   };
   
 } // namespace proxddp
--- include/proxddp/core/problem.hxx
+++ include/proxddp/core/problem.hxx
@@ -6,16 +6,22 @@
 namespace proxddp
 {
   template<typename Scalar>
-  void ShootingProblemTpl<Scalar>::addStage(const shared_ptr<StageModelTpl<Scalar>>& new_stage)
+  void ShootingProblemTpl<Scalar>::addStage(const StageModel& new_stage)
   {
     stages_.push_back(new_stage);
   }
 
   template<typename Scalar>
-  void ShootingProblemTpl<Scalar>::addStage(shared_ptr<StageModelTpl<Scalar>>&& new_stage)
+  void ShootingProblemTpl<Scalar>::addStage(StageModel&& new_stage)
   {
     stages_.push_back(std::move(new_stage));
   }
   
+  template<typename Scalar>
+  inline std::size_t ShootingProblemTpl<Scalar>::numSteps() const
+  {
+    return stages_.size();
+  }
+
 } // namespace proxddp
 
