8713ae464a857c61d6d5b8bb986a18e85c5e7b8a
ManifoldFR <wilson.jallet@polytechnique.org>
2022-06-07 19:33:24 +0200

    working lqr test

--- examples/lqr.cpp
+++ examples/lqr.cpp
@@ -2,9 +2,15 @@
 /// @brief Linear-quadratic regulator
 
 #include "proxddp/core/explicit-dynamics.hpp"
+#include "proxddp/core/shooting-problem.hpp"
 #include "proxddp/utils.hpp"
+#include "proxddp/modelling/costs.hpp"
+#include "proxddp/solver-proxddp.hpp"
 
 #include <proxnlp/modelling/spaces/vector-space.hpp>
+#include <proxnlp/modelling/constraints/negative-orthant.hpp>
+
+#include "proxddp/modelling/box-constraints.hpp"
 
 #include "boost/optional.hpp"
 
@@ -56,23 +62,89 @@ struct LinearDiscreteDynamics : ExplicitDynamicsModelTpl<_Scalar>
 int main()
 {
 
-  const int dim = 3;
-  const int nu = 2;
+  const int dim = 2;
+  const int nu = 1;
   Eigen::MatrixXd A(dim, dim);
   Eigen::MatrixXd B(dim, nu);
+  Eigen::VectorXd c_(dim);
   A.setIdentity();
-  B.setRandom();
+  B << -0.6, 0.3;
+  c_ << 0.1, 0.;
 
-  LinearDiscreteDynamics<double> dynamics(A, B);
+  Eigen::MatrixXd w_x(dim, dim), w_u(nu, nu);
+  w_x.setIdentity();
+  w_u.setIdentity();
+  w_x(0, 0) = 2.1;
+  w_u(0, 0) = 0.06;
 
-  auto x0 = dynamics.out_space().rand();
-  std::size_t nsteps = 10;
+  auto dynptr = std::make_shared<LinearDiscreteDynamics<double>>(A, B, c_);
+  auto& dynamics = *dynptr;
+  fmt::print("matrix A:\n{}\n", dynamics.A_);
+  fmt::print("matrix B:\n{}\n", dynamics.B_);
+  fmt::print("drift  c:\n{}\n", dynamics.c_);
+  const auto& space = dynamics.out_space();
+
+  QuadraticCost<double> rcost(w_x, w_u);
+  ControlBoxFunction<double> ctrl_bounds_fun(dim, nu, -0.1, 0.1);
+  auto ctrl_bounds_cstr = std::make_shared<StageConstraintTpl<double>>(
+    ctrl_bounds_fun,
+    std::make_shared<proxnlp::NegativeOrthant<double>>());
+
+  // Define stage
+
+  StageModelTpl<double> stage(space, nu, rcost, dynamics);
+  // stage.addConstraint(ctrl_bounds_cstr);
+
+  auto x0 = space.rand();
+  x0 << 1., -0.1;
+  ShootingProblemTpl<double> problem(x0);
+
+  std::size_t nsteps = 8;
+
   std::vector<Eigen::VectorXd> us;
   for (std::size_t i = 0; i < nsteps; i++)
   {
     us.push_back(Eigen::VectorXd::Random(nu));
+    problem.addStage(*stage.clone());
   }
+
+  problem.term_cost_ = std::make_shared<decltype(rcost)>(rcost);
   auto xs = rollout(dynamics, x0, us);
+  fmt::print("Initial traj.:\n");
+  for(std::size_t i = 0; i <= nsteps; i++)
+  {
+    fmt::print("x[{:d}] = {}\n", i, xs[i].transpose());
+  }
+
+  double TOL = 1e-6;
+  double mu_init = 1e-4;
+  double rho_init = 0.;
+
+  SolverProxDDP<double> solver(TOL, mu_init, rho_init);
+
+  WorkspaceTpl<double> workspace(problem);
+  ResultsTpl<double> results(problem);
+  assert(results.xs_.size() == nsteps + 1);
+  assert(results.us_.size() == nsteps);
+
+  solver.run(problem, workspace, results, xs, us);
+
+  std::string line_ = "";
+  for (std::size_t i = 0; i < 20; i++)
+  { line_.append("="); }
+  line_.append("\n");
+  for (std::size_t i = 0; i < nsteps + 1; i++)
+  {
+    // fmt::print("x[{:d}] = {}\n", i, results.xs_[i].transpose());
+    fmt::print("x[{:d}] = {}\n", i, workspace.trial_xs_[i].transpose());
+  }
+  for (std::size_t i = 0; i < nsteps; i++)
+  {
+    // fmt::print("u[{:d}] = {}\n", i, results.us_[i].transpose());
+    fmt::print("u[{:d}] = {}\n", i, workspace.trial_us_[i].transpose());
+  }
+  for (std::size_t i = 0; i < nsteps; i++)
+    fmt::print("gains t={:d},\n{}\n", i, workspace.gains_[i]);
 
   return 0;
 }
