357171976790c268a6a3ab4467620f4a2790c36e
ManifoldFR <wilson.jallet@polytechnique.org>
2022-06-07 19:33:07 +0200

    solver-proxddp: update

--- include/proxddp/solver-proxddp.hpp
+++ include/proxddp/solver-proxddp.hpp
@@ -6,6 +6,8 @@
 #include "proxddp/core/solver-workspace.hpp"
 #include "proxddp/core/solver-results.hpp"
 
+#include <proxnlp/constraint-base.hpp>
+
 #include <fmt/color.h>
 #include <fmt/ostream.h>
 
@@ -13,85 +15,246 @@
 
 namespace proxddp
 {
+  template<typename S>
+  struct LinesearchParams
+  {
+    S alpha_min = 1e-7;
+    S directional_derivative_thresh = 1e-13;
+    S armijo_c1 = 1e-4;
+    S ls_beta = 0.5;
+  };
 
   /// @brief Solver.
   template<typename _Scalar>
-  struct SolverTpl
+  struct SolverProxDDP
   {
     using Scalar = _Scalar;
     PROXNLP_DYNAMIC_TYPEDEFS(Scalar);
 
+    using Problem = ShootingProblemTpl<Scalar>;
     using Workspace = WorkspaceTpl<Scalar>;
+    using Results = ResultsTpl<Scalar>;
+    using StageModel = StageModelTpl<Scalar>;
     using value_store_t = internal::value_storage<Scalar>;
     using q_store_t = internal::q_function_storage<Scalar>;
+
+    /// Solver tolerance \f$\epsilon > 0\f$.
+    Scalar target_tolerance = 1e-6;
 
     const Scalar mu_init = 0.01;
     const Scalar rho_init = 0.;
 
+    /// Maximum number \f$N_{\mathrm{max}}\f$ of Newton iterations.
+    const std::size_t MAX_STEPS = 20;
+    const std::size_t MAX_AL_ITERS = 20;
+
     /// Dual proximal/constraint penalty parameter \f$\mu\f$
     Scalar mu_ = mu_init;
+    Scalar mu_inverse_ = 1. / mu_;
+
     /// Primal proximal parameter \f$\rho > 0\f$
     Scalar rho_ = rho_init;
 
-    /// Solver tolerance \f$\epsilon > 0\f$.
-    const Scalar tol_ = 1e-6;
-    
-    /// @brief Perform the Riccati forward pass.
+    const Scalar inner_tol0 = 1.;
+    const Scalar prim_tol0 = 1.;
+
+    const Scalar prim_alpha;
+    const Scalar prim_beta;
+    const Scalar dual_alpha;
+    const Scalar dual_beta;
+
+    Scalar mu_update_factor_ = 0.1;
+
+    /// Subproblem tolerance
+    Scalar inner_tol_;
+    /// Desired primal feasibility
+    Scalar prim_tol;
+
+    LinesearchParams<Scalar> ls_params;
+
+    /// Minimum possible tolerance asked from the solver.
+    const Scalar TOL_MIN = 1e-10;
+
+    SolverProxDDP(const Scalar tol=1e-6,
+              const Scalar mu_init=0.01,
+              const Scalar rho_init=0.,
+              const Scalar prim_alpha=0.1,
+              const Scalar prim_beta=0.9,
+              const Scalar dual_alpha=1.,
+              const Scalar dual_beta=1.
+              )
+      : target_tolerance(tol)
+      , mu_init(mu_init)
+      , rho_init(rho_init)
+      , prim_alpha(prim_alpha)
+      , prim_beta(prim_beta)
+      , dual_alpha(dual_alpha)
+      , dual_beta(dual_beta)
+      {}
+
+    /// @brief Compute the search direction.
     ///
     /// @warning This function assumes \f$\delta x_0\f$ has already been computed!
     /// @returns This computes the primal-dual step \f$(\delta \bfx,\delta \bfu,\delta\bmlam)\f$
-    void forwardPass(const ShootingProblemTpl<Scalar>& problem,
-                     Workspace& workspace) const;
+    void computeDirection(const Problem& problem, Workspace& workspace) const;
 
     /// @brief    Try a step of size \f$\alpha\f$.
     /// @returns  A primal-dual trial point
     ///           \f$(\bfx \oplus\alpha\delta\bfx, \bfu+\alpha\delta\bfu, \bmlam+\alpha\delta\bmlam)\f$
-    void tryStep(const ShootingProblemTpl<Scalar>& problem,
-                 Workspace& workspace,
-                 ResultsTpl<Scalar>& results,
-                 const Scalar alpha) const;
+    void tryStep(const Problem& problem, Workspace& workspace,
+                 Results& results, const Scalar alpha) const;
 
     /// Compute the active sets at each node and multiplier estimates, and projector Jacobian matrices.
-    void computeActiveSetsAndMultipliers(
-      const ShootingProblemTpl<Scalar>& problem,
-      Workspace& workspace,
-      ResultsTpl<Scalar>& results) const;
+    void computeActiveSetsAndMultipliers(const Problem& problem, Workspace& workspace, Results& results) const;
 
     /// @brief    Perform the Riccati backward pass.
     ///
     /// @warning  Compute the derivatives first!
-    void backwardPass(
-      const ShootingProblemTpl<Scalar>& problem,
+    void backwardPass(const Problem& problem, Workspace& workspace, Results& results) const;
+
+    bool run(
+      const Problem& problem,
       Workspace& workspace,
-      ResultsTpl<Scalar>& results) const;
+      Results& results,
+      const std::vector<VectorXs>& xs_init,
+      const std::vector<VectorXs>& us_init)
+    {
+      const std::size_t nsteps = problem.numSteps();
+      assert(xs_init.size() == nsteps + 1);
+      assert(us_init.size() == nsteps);
+
+      results.xs_ = xs_init;
+      results.us_ = us_init;
+      results.xs_[0] = problem.x0_init;
+
+      workspace.prev_xs_ = results.xs_;
+      workspace.prev_us_ = results.us_;
+      workspace.prev_lams_ = results.lams_;
+
+      inner_tol_ = inner_tol0;
+      prim_tol = prim_tol0;
+      this->updateTolerancesOnFailure();
+
+      inner_tol_ = std::max(inner_tol_, target_tolerance);
+
+      bool conv = false;
+
+      std::size_t al_iter;
+      for (al_iter = 0; al_iter < MAX_AL_ITERS; al_iter++)
+      {
+        fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::medium_orchid),
+                   "[AL iter {:>02d}]", al_iter);
+        fmt::print("\n");
+        fmt::print("inner_tol={:.3e} | dual_tol={:.3e} | mu={:.3e} | rho={:.3e}\n",
+                   inner_tol_, prim_tol, mu_, rho_);
+        solverInnerLoop(problem, workspace, results);
+
+        if (workspace.primal_infeasibility <= prim_tol)
+        {
+          this->updateTolerancesOnSuccess();
+          workspace.prev_lams_ = workspace.lams_plus_;
+          if (workspace.primal_infeasibility <= target_tolerance)
+          {
+            conv = true;
+            break;
+          }
+        } else {
+          this->updateALPenalty();
+          this->updateTolerancesOnFailure();
+        }
+
+        inner_tol_ = std::max(inner_tol_, TOL_MIN);
+        prim_tol = std::max(prim_tol, TOL_MIN);
+      }
+
+      if (conv)
+      {
+        fmt::print(fmt::fg(fmt::color::dodger_blue), "Successfully converged.\n");
+      }
+
+      return conv;
+    }
 
     /** @brief    Perform the inner loop of the algorithm (augmented Lagrangian minimization).
      */
     void solverInnerLoop(
-      const ShootingProblemTpl<Scalar>& problem,
+      const Problem& problem,
       Workspace& workspace,
-      ResultsTpl<Scalar>& results)
+      Results& results)
     {
 
+      const std::size_t nsteps = problem.numSteps();
       results.xs_ = workspace.prev_xs_;
       results.us_ = workspace.prev_us_;
       results.lams_ = workspace.prev_lams_;
 
+      assert(results.xs_.size() == nsteps + 1);
+      assert(results.us_.size() == nsteps);
+      assert(results.lams_.size() == nsteps);
+
       std::size_t k = 0;
-      while (k < MAX_ITERS)
+      while (k < MAX_STEPS)
       {
+        fmt::print(fmt::fg(fmt::color::yellow_green), "iter {:>3d}", k);
+        fmt::print("\n");
+        problem.evaluate(results.xs_, results.us_, *workspace.problem_data);
+        problem.computeDerivatives(results.xs_, results.us_, *workspace.problem_data);
 
         backwardPass(problem, workspace, results);
+        workspace.inner_criterion = math::infty_norm(workspace.inner_criterion_by_stage);
 
-        // check stopping criterion
+        fmt::print(" | inner_crit: {:.3e}\n", workspace.inner_criterion);
 
-        forwardPass(problem, workspace);
+        if ((workspace.inner_criterion < inner_tol_))
+        {
+          break;
+        }
 
-        performLinesearch(problem, workspace, results);
+        workspace.dxs_[0].setZero();
+        computeDirection(problem, workspace);
+
+        Scalar alpha_opt = performLinesearch(problem, workspace, results);
+        tryStep(problem, workspace, results, alpha_opt);
+
+        // accept the damn step
+        results.xs_ = workspace.trial_xs_;
+        results.us_ = workspace.trial_us_;
+        results.lams_ = workspace.trial_lams_;
 
         k++;
       }
 
+    }
+
+    /** @brief Compute algorithm termination criteria.
+     * 
+     * @details The termination for \f$u\f$,
+     * \f[
+     *    \| \nabla_u H \|_\infty \leq \omega
+     * \f]
+     * backwardPass() must be called first.
+     */
+    void computeTerminationCriteria(const Problem& problem, Workspace& workspace, const Results& results)
+    {
+      const std::size_t nsteps = problem.numSteps();
+      VectorXs crit_x_vec(nsteps);
+      VectorXs crit_u_vec(nsteps);
+      VectorXs crit_l_vec(nsteps);  // proximal dual infeas.
+
+      workspace.dual_infeasibility = 0.;
+      auto all_qparams = workspace.q_params;
+      for (std::size_t i = 0; i < nsteps; i++)
+      {
+        crit_u_vec((long)i) = math::infty_norm(all_qparams[i].Qu_);
+        crit_x_vec((long)i) = math::infty_norm(all_qparams[i].Qy_);
+        crit_l_vec((long)i) = math::infty_norm( mu_ * (workspace.lams_plus_[i] - results.lams_[i]) );
+      }
+      fmt::print("Qu norms: {}\n", crit_u_vec.transpose());
+      fmt::print("Qy norms: {}\n", crit_x_vec.transpose());
+      fmt::print("proxerr : {}\n", crit_l_vec.transpose());
+
+      if (rho_ == 0.)
+        workspace.dual_infeasibility = std::max(crit_u_vec.maxCoeff(), crit_x_vec.maxCoeff());
 
     }
 
@@ -99,130 +262,46 @@ namespace proxddp
     /// @returns    The optimal step size \f$\alpha^*\f$.
     Scalar performLinesearch(const ShootingProblemTpl<Scalar>& problem,
                              Workspace& workspace,
-                             const ResultsTpl<Scalar>& results) const
+                             const Results& results) const
     {
-
+      return 1.;
     }
 
   protected:
 
-    /// Maximum number \f$N_{\mathrm{max}}\f$ of Newton iterations.
-    const std::size_t MAX_ITERS = 100;
-
     /// @brief  Put together the Q-function parameters and compute the Riccati gains.
     inline void computeGains(
-      const ShootingProblemTpl<Scalar>& problem,
+      const Problem& problem,
       Workspace& workspace,
-      ResultsTpl<Scalar>& results,
-      const std::size_t step)
+      Results& results,
+      const std::size_t step) const;
+
+    void updateTolerancesOnFailure()
     {
-      using StageModel = StageModelTpl<Scalar>;
-      using FunctionData = FunctionDataTpl<Scalar>;
+      prim_tol = prim_tol0 * std::pow(mu_, prim_alpha);
+      inner_tol_ = inner_tol0 * std::pow(mu_, dual_alpha);
+    }
 
-      const StageModel& stage = problem.stages_[step];
-      const std::size_t numc = stage.numConstraints();
+    void updateTolerancesOnSuccess()
+    {
+      prim_tol = prim_tol * std::pow(mu_, prim_beta);
+      inner_tol_ = inner_tol_ * std::pow(mu_, dual_beta);
+    }
 
-      const value_store_t& vnext = workspace.value_params[step];
-      q_store_t& qparams = workspace.q_params[step];
+    void setPenalty(Scalar new_mu)
+    {
+      mu_ = new_mu;
+      mu_inverse_ = 1. / new_mu;
+    }
 
-      const StageDataTpl<Scalar>& stage_data = workspace.problem_data->stage_data[step];
-      const CostDataTpl<Scalar>& cdata = *stage_data.cost_data;
-      const DynamicsDataTpl<Scalar>& dyn_data = *stage_data.dyn_data;
-      using Constraint = StageConstraintTpl<Scalar>;
-
-      int nprim = stage.numPrimal();
-      int ndual = stage.numDual();
-      int nu = stage.nu();
-      int ndx1 = stage.ndx1();
-      int ndx2 = stage.ndx2();
-
-      assert(vnext.storage.rows() == ndx2 + 1);
-      assert(vnext.storage.cols() == ndx2 + 1);
-
-      // Use the contiguous full gradient/jacobian/hessian buffers
-      // to fill in the Q-function derivatives
-      auto qhess_autoadj = qparams.hess_.template selfadjointView<Eigen::Lower>();
-      qparams.storage.setZero();
-
-      qparams.q_2_ = 2 * cdata.value_;
-      qparams.grad_.head(ndx1 + nu) = cdata.grad_;
-      qparams.grad_.tail(ndx2) = vnext.Vx_;
-      qhess_autoadj.topLeftCorner(ndx1 + nu, ndx1 + nu) = cdata.hess_;
-      qhess_autoadj.bottomRightCorner(ndx2, ndx2) = vnext.Vxx_;
-
-      // self-adjoint view to (nprim + ndual) sized block of kkt buffer
-      auto kkt_mat = workspace.getKktView(nprim, ndual);
-      MatrixRef kkt_rhs = workspace.kktRhsFull_.topLeftCorner(nprim + ndual, stage.ndx1() + 1);
-      MatrixRef kkt_jac = kkt_mat.block(nprim, 0, ndual, nprim);
-
-      VectorRef lambda_in = results.lams_[step];
-      VectorRef lamprev = workspace.prev_lams_[step];
-      VectorRef lamplus = workspace.lams_plus_[step];
-      VectorRef lampdal = workspace.lams_pdal_[step];
-
-      // Loop over constraints
-      for (std::size_t i = 0; i < numc; i++)
-      {
-
-        const auto& cstr = stage.constraints_[i];
-        const ConstraintSetBase<Scalar>& cstr_set = cstr->set_;
-        const FunctionData& cstr_data = *stage_data.constraint_data[i];
-
-        // Grab Lagrange multiplier segments
-
-        VectorRef lam_i     = stage.constraints_.getSegmentByConstraint(lambda_in, i);
-        VectorRef lamprev_i = stage.constraints_.getSegmentByConstraint(lamprev,   i);
-        VectorRef lamplus_i = stage.constraints_.getSegmentByConstraint(lamplus,   i);
-        VectorRef lampdal_i = stage.constraints_.getSegmentByConstraint(lampdal,   i);
-
-        // Pre-projection multiplier estimate
-
-        lamplus_i = lamprev_i + (1. / mu_) * cstr_data.value_;
-        MatrixRef J_ = cstr_data.jac_buffer_;
-
-        // compose Jacobian by projector and project multiplier
-
-        cstr_set.applyNormalConeProjectionJacobian(lamplus_i, J_);
-        lamplus_i.noalias() = cstr_set.normalConeProjection(lamplus_i);
-        lampdal_i = 2 * lamplus_i - lambda_in;
-
-        // update multiplier
-
-        qparams.grad_.noalias() += J_.transpose() * lam_i;
-        qhess_autoadj.noalias() += cstr_data.vhp_buffer_;
-        stage.constraints_.getBlockByConstraint(kkt_jac, i) = J_;
-        // kkt_jac.middleRows(cursor, nr) = J_;
-      }
-
-      // blocks: u, y, and dual
-      kkt_rhs.col(0).head(nprim) = qparams.grad_.tail(nprim);
-      kkt_rhs.col(0).tail(ndual) = mu_ * (workspace.lams_plus_[step] - results.lams_);
-
-      // KKT matrix: (u, y)-block = bottom right of qhessadj
-      kkt_mat.topLeftCorner(nprim, nprim) = qhess_autoadj.bottomRightCorner(nprim, nprim);
-      // dual block
-      kkt_mat.bottomRightCorner(ndual, ndual) = - mu_ * MatrixXs::Identity(ndual, ndual);
-
-      /* Compute gains with LDLT */
-      using kkt_mat_t = decltype(kkt_mat);
-      Eigen::LDLT<kkt_mat_t> ldlt_ = kkt_mat.ldlt();
-      workspace.gains_[step] = -kkt_rhs;
-      ldlt_.solveInPlace(workspace.gains_[step]);
-
-
-      /* Value function */
-      value_store_t& v_current = workspace.value_params[step];
-
-      v_current.storage = qparams.storage.topLeftCorner(ndx1 + 1, ndx1 + 1);
-      // v_current.v_2_ = qparams.q_2_;
-      // v_current.Vx_ = qparams.Qx_;
-      // v_current.Vxx_ = qparams.Qxx_;
-      v_current.storage = v_current.storage + kkt_rhs.transpose() * workspace.gains_[step];
+    /// Update the dual proximal penalty.
+    void updateALPenalty()
+    {
+      setPenalty(mu_ * mu_update_factor_);
     }
 
   };
-
-  
+ 
 } // namespace proxddp
 
 #include "proxddp/solver-proxddp.hxx"
--- include/proxddp/solver-proxddp.hxx
+++ include/proxddp/solver-proxddp.hxx
@@ -9,94 +9,191 @@
 namespace proxddp
 {
   template<typename Scalar>
-  void SolverTpl<Scalar>::forwardPass(
-    const ShootingProblemTpl<Scalar>& problem,
+  void SolverProxDDP<Scalar>::computeDirection(
+    const Problem& problem,
     WorkspaceTpl<Scalar>& workspace) const
   {
-    using StageModel = StageModelTpl<Scalar>;
-
-    const std::vector<MatrixXs>& gains = workspace.gains_;
     const std::size_t nsteps = problem.numSteps();
-    assert(gains.size() == nsteps);
 
-    int nu;
-    int ndual;
-    std::size_t i = 0;
-    for (i = 0; i < nsteps; i++)
+    workspace.dxs_[0].setZero();
+    for (std::size_t i = 0; i < nsteps; i++)
     {
       const StageModel& stage = problem.stages_[i];
-      nu = stage.nu();
-      ndual = stage.numDual();
-      VectorXs feedforward(gains[i].leftCols(1));
-      MatrixXs feedback(gains[i].rightCols(stage.ndx1()));
+      // int nu = stage.nu();
+      // int ndual = stage.numDual();
+      VectorRef pd_step = workspace.pd_step_[i + 1];
+      MatrixRef gains = workspace.gains_[i];
+      ConstVectorRef feedforward = gains.col(0);
+      ConstMatrixRef feedback = gains.rightCols(stage.ndx1());
 
-      VectorXs pd_step = feedforward + feedback * workspace.dxs_[i];
-      workspace.dus_[i] = pd_step.head(nu);
-      workspace.dxs_[i + 1] = pd_step.segment(nu, stage.ndx2());
-      workspace.dlams_[i] = pd_step.tail(ndual);
+      pd_step = feedforward + feedback * workspace.dxs_[i];
     }
 
   }
 
   template<typename Scalar>
-  void SolverTpl<Scalar>::computeActiveSetsAndMultipliers(
-    const ShootingProblemTpl<Scalar>& problem,
-    WorkspaceTpl<Scalar>& workspace,
-    ResultsTpl<Scalar>& results) const
+  void SolverProxDDP<Scalar>::computeActiveSetsAndMultipliers(
+    const Problem& problem,
+    Workspace& workspace,
+    Results& results) const
   {
 
   }
   
   template<typename Scalar>
-  void SolverTpl<Scalar>::tryStep(
-    const ShootingProblemTpl<Scalar>& problem,
-    WorkspaceTpl<Scalar>& workspace,
-    ResultsTpl<Scalar>& results,
+  void SolverProxDDP<Scalar>::tryStep(
+    const Problem& problem,
+    Workspace& workspace,
+    Results& results,
     const Scalar alpha) const
   {
-    using StageModel = StageModelTpl<Scalar>;
 
     const std::size_t nsteps = problem.numSteps();
 
-    std::size_t i = 0;
-    for (i = 0; i < nsteps; i++)
+    for (std::size_t i = 0; i < nsteps; i++)
     {
       const StageModel& stage = problem.stages_[i];
       stage.xspace1_.integrate(results.xs_[i], alpha * workspace.dxs_[i], workspace.trial_xs_[i]);
-      stage.uspace.integrate(results.us_[i], alpha * workspace.dus_[i], workspace.trial_us_[i]);
+      stage.uspace_.integrate(results.us_[i], alpha * workspace.dus_[i], workspace.trial_us_[i]);
       workspace.trial_lams_[i] = results.lams_[i] + alpha * workspace.dlams_[i];
+      if (i == nsteps - 1)
+      {
+        stage.xspace2_.integrate(results.xs_[nsteps], alpha * workspace.dxs_[nsteps], workspace.trial_xs_[nsteps]);
+      }
     }
-    i = nsteps - 1;
-    const StageModel& stage_last = problem.stages_[i];
-    stage_last.xspace2_.integrate(results.xs_[i], alpha * workspace.dxs_[i], workspace.trial_xs_[i]);
   }
 
   template<typename Scalar>
-  void SolverTpl<Scalar>::backwardPass(
-    const ShootingProblemTpl<Scalar>& problem,
+  void SolverProxDDP<Scalar>::backwardPass(
+    const Problem& problem,
     Workspace& workspace,
     ResultsTpl<Scalar>& results) const
   {
-    using StageModel = StageModelTpl<Scalar>;
-    using StageData = StageDataTpl<Scalar>;
-
-    const ProblemDataTpl<Scalar>& problem_data = *workspace.problem_data;
+    const ShootingProblemDataTpl<Scalar>& problem_data = *workspace.problem_data;
 
     const std::size_t nsteps = problem.numSteps();
-
 
     /* Terminal node */
     const auto& term_data = *problem_data.term_cost_data;
     value_store_t& term_value = workspace.value_params[nsteps];
 
-    term_value.v_2_ = 2 * term_data->value_;
-    term_value.Vx_ = term_data->Lx_;
-    term_value.Vxx_ = term_data->Lxx_;
+    term_value.v_2_ = 2 * term_data.value_;
+    term_value.Vx_ = term_data.Lx_;
+    term_value.Vxx_ = term_data.Lxx_;
 
-    for (std::size_t step = nsteps - 1; step > 0; step--)
+    for (std::size_t i = 0; i < nsteps; i++)
     {
-      computeGains(problem, workspace, step);
+      computeGains(problem, workspace, results, nsteps - i - 1);
     }
+  }
+
+  template<typename Scalar>
+  void SolverProxDDP<Scalar>::computeGains(
+    const ShootingProblemTpl<Scalar>& problem,
+    Workspace& workspace,
+    ResultsTpl<Scalar>& results,
+    const std::size_t step) const
+  {
+    using FunctionData = FunctionDataTpl<Scalar>;
+
+    const StageModel& stage = problem.stages_[step];
+    const std::size_t numc = stage.numConstraints();
+
+    const value_store_t& vnext = workspace.value_params[step + 1];
+    q_store_t& q_param = workspace.q_params[step];
+
+    StageDataTpl<Scalar>& stage_data = *workspace.problem_data->stage_data[step];
+    const CostDataAbstract<Scalar>& cdata = *stage_data.cost_data;
+
+    int nprim = stage.numPrimal();
+    int ndual = stage.numDual();
+    int ndx1 = stage.ndx1();
+    int nu = stage.nu();
+    int ndx2 = stage.ndx2();
+
+    assert(vnext.storage.rows() == ndx2 + 1);
+    assert(vnext.storage.cols() == ndx2 + 1);
+
+    // Use the contiguous full gradient/jacobian/hessian buffers
+    // to fill in the Q-function derivatives
+    q_param.storage.setZero();
+
+    q_param.q_2_ = 2 * cdata.value_;
+    q_param.grad_.head(ndx1 + nu) = cdata.grad_;
+    q_param.grad_.tail(ndx2) = vnext.Vx_;
+    q_param.hess_.topLeftCorner(ndx1 + nu, ndx1 + nu) = cdata.hess_;
+    q_param.hess_.bottomRightCorner(ndx2, ndx2) = vnext.Vxx_;
+
+    // self-adjoint view to (nprim + ndual) sized block of kkt buffer
+    MatrixRef kkt_mat = workspace.getKktView(nprim, ndual);
+    MatrixRef kkt_rhs = workspace.getKktRhs(nprim, ndual, ndx1);
+    MatrixRef kkt_jac = kkt_mat.block(nprim, 0, ndual, nprim);
+
+    VectorRef rhs_0 = kkt_rhs.col(0);
+    MatrixRef rhs_D = kkt_rhs.rightCols(ndx1);
+
+    VectorRef lambda_in = results.lams_[step];
+    VectorRef lamprev = workspace.prev_lams_[step];
+    VectorRef lamplus = workspace.lams_plus_[step];
+    VectorRef lampdal = workspace.lams_pdal_[step];
+
+    // Loop over constraints
+    for (std::size_t i = 0; i < numc; i++)
+    {
+      const auto& cstr = stage.constraints_manager[i];
+      const auto& cstr_set = *cstr->set_;
+      FunctionData& cstr_data = *stage_data.constraint_data[i];
+      MatrixRef cstr_jac = cstr_data.jac_buffer_;
+
+      // Grab Lagrange multiplier segments
+
+      VectorRef lam_i     = stage.constraints_manager.getSegmentByConstraint(lambda_in, i);
+      VectorRef lamprev_i = stage.constraints_manager.getSegmentByConstraint(lamprev,   i);
+      VectorRef lamplus_i = stage.constraints_manager.getSegmentByConstraint(lamplus,   i);
+      VectorRef lampdal_i = stage.constraints_manager.getSegmentByConstraint(lampdal,   i);
+
+      assert(cstr_jac.rows() == cstr->nr());
+      assert(cstr_jac.cols() == ndx1 + nprim);
+
+      // compose Jacobian by projector and project multiplier
+      lamplus_i = lamprev_i + mu_inverse_ * cstr_data.value_;
+      cstr_set.applyNormalConeProjectionJacobian(lamplus_i, cstr_jac);
+      lamplus_i.noalias() = cstr_set.normalConeProjection(lamplus_i);
+      lampdal_i = 2 * lamplus_i - lam_i;
+
+      q_param.grad_.noalias() += cstr_jac.transpose() * lam_i;
+      q_param.hess_.noalias() += cstr_data.vhp_buffer_;
+
+      // update the KKT jacobian columns
+      stage.constraints_manager.getBlockByConstraint(kkt_jac, i) = cstr_jac.rightCols(nprim);
+      stage.constraints_manager.getBlockByConstraint(rhs_D.bottomRows(ndual), i) = cstr_jac.leftCols(ndx1);
+    }
+
+    // blocks: u, y, and dual
+    rhs_0.head(nprim) = q_param.grad_.tail(nprim);
+    rhs_0.tail(ndual) = mu_ * (workspace.lams_plus_[step] - results.lams_[step]);
+
+    rhs_D.middleRows(0,  nu) = q_param.Qxu_.transpose();
+    rhs_D.middleRows(nu, ndx2) = q_param.Qxy_.transpose();
+
+    // KKT matrix: (u, y)-block = bottom right of q hessian
+    kkt_mat.topLeftCorner(nprim, nprim) = q_param.hess_.bottomRightCorner(nprim, nprim);
+    kkt_mat.bottomRightCorner(ndual, ndual).diagonal().array() = -mu_;
+
+    auto kkt_mat_view = kkt_mat.template selfadjointView<Eigen::Lower>();
+
+    workspace.inner_criterion_by_stage((long)step) = math::infty_norm(rhs_0);
+
+    /* Compute gains with LDLT */
+    auto ldlt_ = kkt_mat_view.ldlt();
+    workspace.gains_[step] = -kkt_rhs;
+    ldlt_.solveInPlace(workspace.gains_[step]);
+
+    /* Value function */
+    value_store_t& v_current = workspace.value_params[step];
+    v_current.storage = \
+      q_param.storage.topLeftCorner(ndx1 + 1, ndx1 + 1) +\
+      kkt_rhs.transpose() * workspace.gains_[step];
   }
 
 } // namespace proxddp
