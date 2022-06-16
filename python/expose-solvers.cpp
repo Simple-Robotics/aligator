4f3d56308644b47e1297a5664087a019e85cf964
ManifoldFR <wilson.jallet@polytechnique.org>
2022-06-16 19:55:56 +0200

    python/solver: update ProxDDP class ctor

--- python/expose-solvers.cpp
+++ python/expose-solvers.cpp
@@ -37,20 +37,30 @@ namespace proxddp
 
       using SolverType = SolverProxDDP<Scalar>;
 
+      bp::enum_<MultiplierUpdateMode>("MultiplierUpdateMode", "Enum for the kind of multiplier update to use.")
+        .value("NEWTON", MultiplierUpdateMode::NEWTON)
+        .value("PRIMAL", MultiplierUpdateMode::PRIMAL)
+        .value("PDAL", MultiplierUpdateMode::PDAL)
+        ;
+
       bp::class_<SolverType>(
         "ProxDDP",
         bp::init< Scalar
                 , Scalar
-                , Scalar>(
-                  bp::args("self", "tol", "mu_init", "rho_init")
-                )
-      )
+                , Scalar
+                , Scalar
+                , Scalar>((
+                  "self", "tol", "mu_init", "rho_init", bp::arg("prim_alpha") = 0.1, bp::arg("prim_beta") = 0.9)
+                  ))
         .def_readwrite("target_tol", &SolverType::target_tolerance, "Desired tolerance.")
         .def_readonly("mu_init",  &SolverType::mu_init,  "mu_init")
         .def_readonly("rho_init", &SolverType::rho_init, "rho_init")
-        .def("run", &SolverType::run,
-             bp::args("self", "problem", "workspace", "results",
-                      "xs_init", "us_init"),
+        .def_readwrite("mu_factor",  &SolverType::mu_update_factor_)
+        .def_readwrite("rho_factor", &SolverType::rho_update_factor_)
+        .def_readwrite("multiplier_update_mode", &SolverType::mul_update_mode)
+        .def("run",
+             &SolverType::run,
+             bp::args("self", "problem", "workspace", "results", "xs_init", "us_init"),
              "Run the solver.")
         ;
     }
