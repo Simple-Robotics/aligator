2453b67a9f7fb0f9a2e1e69591dd3e7490283264
ManifoldFR <wilson.jallet@polytechnique.org>
2022-06-28 19:20:53 +0200

    update custom boost.python macros

--- python/macros.hpp
+++ python/macros.hpp
@@ -3,17 +3,20 @@
 #pragma once
 
 #include <type_traits>
-#include <fmt/core.h>
+#include <fmt/format.h>
+#include <stdexcept>
+
 
 namespace proxddp { namespace python {
   namespace internal
   {
+    template<bool B, class T = void>
+    using enable_if_t = typename std::enable_if<B,T>::type;
+
     /// Template function enabled when template arg ret_type is void.
     /// In this case, suppress the return.
-    /// Used in 
     template<typename ret_type, typename T>
-    typename std::enable_if<std::is_same<ret_type, void>::value, void>::type
-    suppress_if_void(T&&) {}
+    enable_if_t<std::is_same<ret_type, void>::value> suppress_if_void(T&&) {}
 
     template<typename T> T suppress_if_void(T&& o) { return std::forward<T>(o); }
     
@@ -22,22 +25,29 @@ namespace proxddp { namespace python {
 } // namespace proxddp
 
 
-#define PROXDDP_PYTHON_OVERRIDE_IMPL(ret_type, pyname, ...)  \
-  if (bp::override fo = this->get_override(pyname))          \
-  { return ::proxddp::python::internal::suppress_if_void<ret_type>(fo(__VA_ARGS__));}  \
+#define PROXDDP_PYTHON_OVERRIDE_IMPL(ret_type, pyname, ...)                          \
+  do {                                                                               \
+    bp::override fo = this->get_override(pyname);                                    \
+    if (fo)                                                                          \
+    {                                                                                \
+      auto o = fo(__VA_ARGS__);                                                      \
+      return ::proxddp::python::internal::suppress_if_void<ret_type>(std::move(o));  \
+    }                                                                                \
+  } while(false)
 
-/** @def PROXDDP_PYTHON_OVERRIDE_PURE(ret_type, pyname, ...)
+/** 
+ * @def PROXDDP_PYTHON_OVERRIDE_PURE(ret_type, pyname, ...)
  * @brief Define the body of a virtual function override. This is meant
  *        to reduce boilerplate code when exposing virtual member functions.
  */
-#define PROXDDP_PYTHON_OVERRIDE_PURE(ret_type, pyname, ...)      \
-  bp::override fo = this->get_override(pyname);                  \
-  return ::proxddp::python::internal::suppress_if_void<ret_type>(fo(__VA_ARGS__));
+#define PROXDDP_PYTHON_OVERRIDE_PURE(ret_type, pyname, ...)                                   \
+  PROXDDP_PYTHON_OVERRIDE_IMPL(ret_type, pyname, __VA_ARGS__);                                \
+  throw std::runtime_error(fmt::format("Tried to call pure virtual function {:s}.", pyname))
 
 /**
- * @def
- * @brief Define the body of a non-pure virtual function override.
+ * @def PROXDDP_PYTHON_OVERRIDE(ret_type, cname, fname, ...)
+ * @copybrief PROXDDP_PYTHON_OVERRIDE_PURE()
  */
-#define PROXDDP_PYTHON_OVERRIDE(ret_type, cname, fname, ...)  \
-  PROXDDP_PYTHON_OVERRIDE_IMPL(ret_type, #fname, __VA_ARGS__) \
+#define PROXDDP_PYTHON_OVERRIDE(ret_type, cname, fname, ...)                         \
+  PROXDDP_PYTHON_OVERRIDE_IMPL(ret_type, #fname, __VA_ARGS__);                       \
   return cname::fname(__VA_ARGS__)
