9527ee25626e73ac194a9359dc199e99aaad67b2
ManifoldFR <wilson.jallet@polytechnique.org>
2022-07-01 19:28:05 +0200

    integrators/semi-euler: fix dForward()'s loops

--- include/proxddp/modelling/dynamics/integrator-semi-impl-euler.hxx
+++ include/proxddp/modelling/dynamics/integrator-semi-impl-euler.hxx
@@ -39,6 +39,7 @@ namespace proxddp
       ODEDataTpl<Scalar>& cdata = static_cast<ODEDataTpl<Scalar>&>(*d.continuous_data);
       int ndx =  this->ndx1;
       int nu =  this->nu;
+      const int ndx_2 = ndx / 2;
       MatrixXs Jxtemp_(ndx, ndx), Jutemp_(ndx,nu);
       this->ode_->dForward(x, u, cdata);
       // dv_dx and dv_du are same as euler explicit
@@ -49,23 +50,16 @@ namespace proxddp
       this->out_space().Jintegrate(x, d.dx_, d.Jtmp_xnext, 0);
       d.Jx_ = d.Jtmp_xnext + d.Jx_; 
       // dq_dx and dq_du needs to be modified
-      for(int i = 0; i<ndx/2; i++){
-        Jxtemp_(i) = timestep_ *d.Jx_(ndx/2+i);
-        Jxtemp_(i+ndx/2) = timestep_ *cdata.Jx_(ndx/2+i);
-      }
-      for(int i = 0; i<ndx/2; i++){
-        Jutemp_(i) = timestep_ *d.Ju_(ndx/2+i);
-        Jutemp_(i+ndx/2) = timestep_ *cdata.Ju_(ndx/2+i);
-      }
+      Jxtemp_.topRows(ndx_2) = timestep_ * d.Jx_.bottomRows(ndx_2);
+      Jxtemp_.bottomRows(ndx_2) = timestep_ * cdata.Jx_.bottomRows(ndx_2);
+      Jutemp_.topRows(ndx_2) = timestep_ * d.Ju_.bottomRows(ndx_2);
+      Jutemp_.bottomRows(ndx_2) = timestep_ * cdata.Ju_.bottomRows(ndx_2);
+
       this->out_space().JintegrateTransport(x, d.dx_, Jxtemp_, 1);
       this->out_space().JintegrateTransport(x, d.dx_, Jutemp_, 1);
       Jxtemp_ = d.Jtmp_xnext + Jxtemp_; 
-      for(int i = 0; i<ndx/2; i++){
-        d.Jx_(i) = Jxtemp_(i);
-      }
-      for(int i = 0; i<ndx/2; i++){
-        d.Ju_(i) = Jutemp_(i);
-      }
+      d.Jx_.topRows(ndx_2) = Jxtemp_.topRows(ndx_2);
+      d.Ju_.topRows(ndx_2) = Jutemp_.topRows(ndx_2);
     }
   } // namespace dynamics
   
