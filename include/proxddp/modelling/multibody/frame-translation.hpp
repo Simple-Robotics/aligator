11c6a057b82533826bd55f9d2141c8c78aa3e4a5
ManifoldFR <wilson.jallet@polytechnique.org>
2022-07-18 19:43:19 +0200

    fix includes from new files

--- include/proxddp/modelling/multibody/frame-translation.hpp
+++ include/proxddp/modelling/multibody/frame-translation.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "proxddp/core/function.hpp"
+#include "proxddp/core/function-abstract.hpp"
 
 #include <pinocchio/multibody/model.hpp>
 #include <pinocchio/multibody/frame.hpp>
@@ -25,17 +25,17 @@ public:
   pinocchio::FrameIndex pin_frame_id_;
 
   FrameTranslationResidualTpl(const int ndx, const int nu,
-                            const shared_ptr<Model> &model, const VectorXs &frame_trans,
-                            const pinocchio::FrameIndex id)
-      : Base(ndx, nu, 3), pin_model_(model), pin_frame_id_(id), p_ref_(frame_trans){}
+                              const shared_ptr<Model> &model,
+                              const VectorXs &frame_trans,
+                              const pinocchio::FrameIndex id)
+      : Base(ndx, nu, 3), pin_model_(model), pin_frame_id_(id),
+        p_ref_(frame_trans) {}
 
   pinocchio::FrameIndex getFrameId() const { return pin_frame_id_; }
   void setFrameId(const std::size_t id) { pin_frame_id_ = id; }
 
   const VectorXs &getReference() const { return p_ref_; }
-  void setReference(const VectorXs &p_new) {
-    p_ref_ = p_new;
-  }
+  void setReference(const VectorXs &p_new) { p_ref_ = p_new; }
 
   void evaluate(const ConstVectorRef &x, const ConstVectorRef &u,
                 const ConstVectorRef &y, BaseData &data) const;
