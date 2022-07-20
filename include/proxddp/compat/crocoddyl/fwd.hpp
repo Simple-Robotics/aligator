5366cf02ca71c83a91067ec1df87127d2c01bd4b
ManifoldFR <wilson.jallet@polytechnique.org>
2022-07-20 16:17:56 +0200

    compat/crocoddyl: add fwd declarations

---
+++ include/proxddp/compat/crocoddyl/fwd.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace proxddp {
+namespace compat {
+namespace croc {
+
+template <typename Scalar> struct StateWrapperTpl;
+
+template <typename Scalar> struct CrocCostDataWrapperTpl;
+
+template <typename Scalar> struct CrocCostModelWrapperTpl;
+
+template <typename Scalar> struct DynamicsDataWrapperTpl;
+
+template <typename Scalar> struct CrocActionModelWrapperTpl;
+
+template <typename Scalar> struct CrocActionDataWrapperTpl;
+
+} // namespace croc
+} // namespace compat
+} // namespace proxddp
