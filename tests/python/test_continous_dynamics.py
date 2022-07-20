7baead486da9caf71ebe4cbddb9bdc28614325a3
ManifoldFR <wilson.jallet@polytechnique.org>
2022-07-20 18:19:25 +0200

    test_continuous_dynamics: fix check which relies on crocoddyl

--- tests/python/test_continous_dynamics.py
+++ tests/python/test_continous_dynamics.py
@@ -41,20 +41,23 @@ def test_multibody_free():
     ode.dForward(x0, u0, data)
 
     # compare with croc
-    import crocoddyl as cc
+    try:
+        import crocoddyl as cc
 
-    state_ = cc.StateMultibody(model)
-    actuation_ = cc.ActuationModelFull(state_)
-    cost_ = cc.CostModelSum(state_, nu)
-    cm = cc.DifferentialActionModelFreeFwdDynamics(state_, actuation_, cost_)
-    cm_data = cm.createData()
-    cm.calc(cm_data, x0, u0)
-    cm.calcDiff(cm_data, x0, u0)
+        state_ = cc.StateMultibody(model)
+        actuation_ = cc.ActuationModelFull(state_)
+        cost_ = cc.CostModelSum(state_, nu)
+        cm = cc.DifferentialActionModelFreeFwdDynamics(state_, actuation_, cost_)
+        cm_data = cm.createData()
+        cm.calc(cm_data, x0, u0)
+        cm.calcDiff(cm_data, x0, u0)
 
-    err_x = abs(cm_data.Fx - data.Jx[model.nv :])
-    err_u = abs(cm_data.Fu - data.Ju[model.nv :])
-    assert np.allclose(err_x, 0.0)
-    assert np.allclose(err_u, 0.0)
+        err_x = abs(cm_data.Fx - data.Jx[model.nv :])
+        err_u = abs(cm_data.Fu - data.Ju[model.nv :])
+        assert np.allclose(err_x, 0.0)
+        assert np.allclose(err_u, 0.0)
+    except ImportError:
+        pass
 
 
 def test_linear():
