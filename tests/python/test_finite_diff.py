72c449be76a9f15ef4ce63658f8fbd42f4e743af
fabinsch <55981657+fabinsch@users.noreply.github.com>
2022-07-29 19:13:27 +0200

    split unittest into two functions
    - create two independent tests
    - skip part of the test if pinocchio cannot be imported (as we started to do this in https://github.com/Simple-Robotics/proxddp/commit/e5a50e8e2de4ce191f0c476d1d7d58dc8a50dc9a)

--- tests/python/test_finite_diff.py
+++ tests/python/test_finite_diff.py
@@ -5,10 +5,9 @@ from proxddp import (
     manifolds,
 )
 import numpy as np
-import pinocchio as pin
 
 
-def test_compute_jac():
+def test_compute_jac_vs():
     nx = 2
     nu = 5
     space = manifolds.VectorSpace(nx)
@@ -27,24 +26,29 @@ def test_compute_jac():
     assert np.allclose(fdata1.Jx, fdata1b.Jx, 1e-2)
     assert np.allclose(fdata1.Ju, fdata1b.Ju, 1e-2)
     assert np.allclose(fdata1.Jy, fdata1b.Jy, 1e-2)
-
-    model = pin.buildSampleModelHumanoid()
-    space = manifolds.MultibodyConfiguration(model)
-    nx = space.nx
-    nu = 3
-    x_tar = space.neutral()
-    fun2 = StateErrorResidual(space, nu, x_tar)
-    fdata2 = fun2.createData()
-    fun2_fd = FiniteDifferenceHelper(space, fun2, 1e-3)
-    fdata2b = fun2_fd.createData()
-    x0 = np.ones(nx)
-    u0 = 0.6 * np.ones(nu)
-    fun2.evaluate(x0, u0, x0, fdata2)
-    fun2_fd.evaluate(x0, u0, x0, fdata2b)
-    assert np.allclose(fdata2.value, fdata2b.value, 1e-2)
-    fun2.computeJacobians(x0, u0, x0, fdata2)
-    fun2_fd.computeJacobians(x0, u0, x0, fdata2b)
-    assert np.allclose(fdata2.Jx, fdata2b.Jx, 1e-2)
-    assert np.allclose(fdata2.Ju, fdata2b.Ju, 1e-2)
-    assert np.allclose(fdata2.Jy, fdata2b.Jy, 1e-2)
-    return
+    
+def test_compute_jac_multibody():
+    try:
+        import pinocchio as pin
+        model = pin.buildSampleModelHumanoid()
+        space = manifolds.MultibodyConfiguration(model)
+        nx = space.nx
+        nu = 3
+        x_tar = space.neutral()
+        fun2 = StateErrorResidual(space, nu, x_tar)
+        fdata2 = fun2.createData()
+        fun2_fd = FiniteDifferenceHelper(space, fun2, 1e-3)
+        fdata2b = fun2_fd.createData()
+        x0 = np.ones(nx)
+        u0 = 0.6 * np.ones(nu)
+        fun2.evaluate(x0, u0, x0, fdata2)
+        fun2_fd.evaluate(x0, u0, x0, fdata2b)
+        assert np.allclose(fdata2.value, fdata2b.value, 1e-2)
+        fun2.computeJacobians(x0, u0, x0, fdata2)
+        fun2_fd.computeJacobians(x0, u0, x0, fdata2b)
+        assert np.allclose(fdata2.Jx, fdata2b.Jx, 1e-2)
+        assert np.allclose(fdata2.Ju, fdata2b.Ju, 1e-2)
+        assert np.allclose(fdata2.Jy, fdata2b.Jy, 1e-2)
+        return
+    except ImportError:
+        pass
