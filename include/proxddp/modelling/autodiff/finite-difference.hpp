c91e4e35afb45ab570d7a6b9ca9f2f014b155117
quentinldc <quentin.le-lidec@inria.fr>
2022-07-29 17:12:37 +0200

    finite differences for computeJacobians of StageFunction

--- include/proxddp/modelling/autodiff/finite-difference.hpp
+++ include/proxddp/modelling/autodiff/finite-difference.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "proxddp/core/function-abstract.hpp"
+#include <proxnlp/manifold-base.hpp>
 
 namespace proxddp {
 namespace autodiff {
@@ -30,7 +31,8 @@ template <typename _Scalar>
 struct finite_difference_impl<_Scalar, TOC1> : virtual StageFunctionTpl<_Scalar> {
   PROXNLP_FUNCTION_TYPEDEFS(_Scalar);
   using Base = StageFunctionTpl<_Scalar>;
-  using Base::computeJacobians;
+  using BaseData = FunctionDataTpl<_Scalar>;
+  // using Base::computeJacobians;
 
   const ManifoldAbstractTpl<_Scalar> &space;
   const Base &func;
@@ -38,21 +40,67 @@ struct finite_difference_impl<_Scalar, TOC1> : virtual StageFunctionTpl<_Scalar>
 
   finite_difference_impl(const ManifoldAbstractTpl<_Scalar> &space,
                          const Base &func, const _Scalar fd_eps)
-      : Base(func.ndx1(), func.nu(), func.ndx2(), func.nr()), space(space),
+      : Base(func.ndx1, func.nu, func.ndx2, func.nr), space(space),
         func(func), fd_eps(fd_eps) {}
 
-  void computeJacobians(const ConstVectorRef &x, MatrixRef Jout) const override {
-    VectorXs ei(func.ndx());
-    VectorXs xplus = space.neutral();
-    VectorXs xminus = space.neutral();
-    ei.setZero();
-    for (int i = 0; i < func.ndx(); i++) {
-      ei(i) = fd_eps;
-      space.integrate(x, ei, xplus);
-      space.integrate(x, -ei, xminus);
-      Jout.col(i) = (func(xplus) - func(xminus)) / (2 * fd_eps);
-      ei(i) = 0.;
+  void evaluate(const ConstVectorRef &x, const ConstVectorRef &u,
+                        const ConstVectorRef &y, BaseData &data) const override{
+    func.evaluate(x, u, y, data);
+  }
+
+  void computeJacobians(const ConstVectorRef &x, const ConstVectorRef &u,
+                                const ConstVectorRef &y, BaseData &data) const override {
+    VectorXs exi(func.ndx1);
+    VectorXs xplus = x;
+    VectorXs xminus = x;
+    VectorXs vplus(func.nr);
+    VectorXs vminus(func.nr);
+    exi.setZero();
+    for (int i = 0; i < func.ndx1; i++) {
+      exi(i) = fd_eps;
+      space.integrate(x, exi, xplus);
+      space.integrate(x, -exi, xminus);
+      func.evaluate(xplus, u, y, data);
+      vplus = data.value_;
+      func.evaluate(xminus, u, y, data);
+      vminus = data.value_;
+      data.Jx_.col(i) = (vplus - vminus) / (2 * fd_eps);
+      exi(i) = 0.;
     }
+    VectorXs eyi(func.ndx2);
+    VectorXs yplus = y;
+    VectorXs yminus = y;
+    eyi.setZero();
+    for (int i = 0; i < func.ndx2; i++){
+      eyi(i) = fd_eps;
+      space.integrate(y, eyi, yplus);
+      space.integrate(y, -eyi, yminus);
+      func.evaluate(x, u, yplus, data);
+      vplus = data.value_; 
+      func.evaluate(x, u, yminus, data);
+      vminus = data.value_;
+      data.Jy_.col(i) = (vplus - vminus) / (2 * fd_eps);
+      eyi(i) = 0.;
+    }
+    VectorXs eui(func.nu);
+    VectorXs uplus = u;
+    VectorXs uminus= u;
+    for (int i = 0; i < func.nu; i++) {
+      eui(i) = fd_eps;
+      space.integrate(u, eui, uplus);
+      space.integrate(u, -eui, uminus);
+      func.evaluate(x, uplus, y, data);
+      vplus = data.value_;
+      func.evaluate(x, uminus, y, data);
+      vminus = data.value_;
+      data.Ju_.col(i) = (vplus - vminus) / (2 * fd_eps);
+      eui(i) = 0.;
+    }
+  }
+
+  void computeVectorHessianProducts(const ConstVectorRef &x, const ConstVectorRef &u,const ConstVectorRef &y,
+                                            const ConstVectorRef &lbda, BaseData &data) const{
+    func.computeVectorHessianProducts(x, u, y, lbda, data);
   }
 };
 
@@ -72,15 +120,17 @@ struct finite_difference_wrapper<_Scalar, TOC1>
   using InputType = StageFunctionTpl<Scalar>;
   using OutType = StageFunctionTpl<Scalar>;
   using Base = internal::finite_difference_impl<Scalar, TOC1>;
+  using Base::evaluate;
   using Base::computeJacobians;
+  using Base::computeVectorHessianProducts;
 
   PROXNLP_FUNCTION_TYPEDEFS(_Scalar);
 
   finite_difference_wrapper(const ManifoldAbstractTpl<Scalar> &space,
                             const InputType &func, const Scalar fd_eps)
-      : OutType(func.ndx1(), func.nu(), func.ndx2(), func.nr()), Base(space, func, fd_eps) {}
+      : OutType(func.ndx1, func.nu, func.ndx2, func.nr), Base(space, func, fd_eps) {}
 
-  ReturnType operator()(const ConstVectorRef &x) const { return this->func(x); }
+  // ReturnType operator()(const ConstVectorRef &x) const { return this->func(x); }
 };
 
 } // namespace autodiff
