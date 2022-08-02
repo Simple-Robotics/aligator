791cbb687232346006e16740ad01e46379b8ddfe
ManifoldFR <wilson.jallet@polytechnique.org>
2022-08-02 13:10:09 +0200

    add PROXDDP_RAISE_IF_NAN macro

--- include/proxddp/utils/exceptions.hpp
+++ include/proxddp/utils/exceptions.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "proxddp/math.hpp"
 #include <stdexcept>
 #include <fmt/core.h>
 
@@ -8,5 +9,9 @@
     throw std::runtime_error(                                                  \
         fmt::format("{}({}): {}", __FILE__, __LINE__, msg));                   \
   }
+
+#define PROXDDP_RAISE_IF_NAN(value)                                            \
+  if (::proxddp::math::checkScalar(value))                                     \
+  proxddp_runtime_error("encountered NaN.\n")
 
 namespace proxddp {} // namespace proxddp
