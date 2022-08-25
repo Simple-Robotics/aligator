6df60bcb1dc9d5ee9bcb3e1cc13c4e6b7cec22dd
ManifoldFR <wilson.jallet@polytechnique.org>
2022-08-25 15:48:07 +0200

    proxddp: fix initialization of mu in run()

--- include/proxddp/core/solver-proxddp.hxx
+++ include/proxddp/core/solver-proxddp.hxx
@@ -339,7 +339,7 @@ bool SolverProxDDP<Scalar>::run(const Problem &problem,
   logger.active = (verbose_ > 0);
   logger.start();
 
-  this->mu_penal_ = mu_init;
+  this->setPenalty(mu_init);
   workspace.prev_xs_ = results.xs_;
   workspace.prev_us_ = results.us_;
   workspace.prev_lams_ = results.lams_;
