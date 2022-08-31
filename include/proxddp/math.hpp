be4253221e8d36e0afffd587d4e8acc83e15468f
ManifoldFR <wilson.jallet@polytechnique.org>
2022-08-31 15:18:40 +0200

    account for proxnlp update (where checkScalar and scalar_close were moved)

--- include/proxddp/math.hpp
+++ include/proxddp/math.hpp
@@ -13,19 +13,5 @@ namespace math {
 
 using namespace proxnlp::math;
 
-/// @brief Check that a scalar is neither inf, nor NaN.
-template <typename Scalar> inline bool checkScalar(const Scalar value) {
-  return std::isnan(value) || std::isinf(value);
-}
-
-/**
- * @brief Tests whether @p a and @p b are close, within absolute and relative
- * precision @p prec.
- */
-template <typename Scalar>
-bool scalar_close(const Scalar a, const Scalar b, const Scalar prec) {
-  return std::abs(a - b) < prec * (1 + std::max(std::abs(a), std::abs(b)));
-}
-
 } // namespace math
 } // namespace proxddp
