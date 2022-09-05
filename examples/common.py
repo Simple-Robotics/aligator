d6415cf036310fde6c6b723a995bd1099995004a
ManifoldFR <wilson.jallet@polytechnique.org>
2022-09-05 18:34:36 +0200

    talos example

--- examples/common.py
+++ examples/common.py
@@ -2,6 +2,8 @@
 Common definitions for examples.
 """
 import tap
+import numpy as np
+import pinocchio as pin
 from typing import Literal
 
 integrator_choices = Literal["euler", "semieuler", "midpoint", "rk2"]
@@ -12,3 +14,15 @@ class ArgsBase(tap.Tap):
     record: bool = False  # record video
     integrator: integrator_choices = "semieuler"
     """Numerical integrator to use"""
+
+
+def get_endpoint(rmodel, rdata, q: np.ndarray, tool_id: int):
+    pin.framesForwardKinematics(rmodel, rdata, q)
+    return rdata.oMf[tool_id].translation.copy()
+
+
+def get_endpoint_traj(rmodel, rdata, xs: list[np.ndarray], tool_id: int):
+    pts = []
+    for i in range(len(xs)):
+        pts.append(get_endpoint(rmodel, rdata, xs[i][: rmodel.nq], tool_id))
+    return np.array(pts)
--- examples/talos_arms.py
+++ examples/talos_arms.py
@@ -3,8 +3,10 @@ import proxddp
 import pinocchio as pin
 import meshcat_utils as msu
 import example_robot_data as erd
+import matplotlib.pyplot as plt
 
 from proxddp import manifolds, dynamics, constraints
+from common import get_endpoint_traj
 
 
 def loadTalos():
@@ -18,8 +20,8 @@ def loadTalos():
 
 
 robot = loadTalos()
-rmodel = robot.model
-rdata = robot.data
+rmodel: pin.Model = robot.model
+rdata: pin.Data = robot.data
 nv = rmodel.nv
 nu = nv
 
@@ -27,15 +29,16 @@ nu = nv
 space = manifolds.MultibodyPhaseSpace(rmodel)
 
 x0 = space.neutral()
-x_hsit = rmodel.referenceConfigurations["half_sitting"]
-print(x_hsit)
 
-Tf = 1.2
-dt = 0.02
+Tf = 1.1
+dt = 0.01
 nsteps = int(Tf / dt)
 
-w_x = np.eye(space.ndx) * 0.1
-w_u = np.eye(nu) * 1e-3
+w_x = np.ones(space.ndx) * 0.1
+w_x[[0, 1]] = 1.0
+w_x[nv:] = 1e-2
+w_x = np.diag(w_x)
+w_u = np.eye(nu) * 1e-4
 act_matrix = np.eye(nu)
 
 ode = dynamics.MultibodyFreeFwdDynamics(space, act_matrix)
@@ -46,17 +49,29 @@ rcost.addCost(proxddp.QuadraticCost(w_x, w_u), dt)
 
 stm = proxddp.StageModel(space, nu, rcost, dyn_model)
 
-term_cost = proxddp.QuadraticResidualCost(
-    proxddp.StateErrorResidual(space, nu, x_hsit), w_x
-)
+term_cost = proxddp.CostStack(space.ndx, nu)
+term_cost.addCost(proxddp.QuadraticResidualCost(
+    proxddp.StateErrorResidual(space, nu, x0), w_x
+))
+
+tool_id_lh = rmodel.getFrameId("gripper_left_base_link")
+target_ee_pos = np.array([0.4, 0.2, 1.4])
+
+frame_fn_lh = proxddp.FrameTranslationResidual(space.ndx, nu, rmodel, target_ee_pos, tool_id_lh)
+w_x_ee = 30. * np.eye(3)
+frame_fn_cost = proxddp.QuadraticResidualCost(frame_fn_lh, w_x_ee)
+term_cost.addCost(frame_fn_cost)
+
 
 stages = [stm] * nsteps
 problem = proxddp.TrajOptProblem(x0, stages, term_cost)
 
 
-TOL = 1e-3
-MU_INIT = 1e-2
-solver = proxddp.SolverProxDDP(TOL, MU_INIT, 1e-8)
+TOL = 1e-4
+mu_init = 1e-5
+rho_init = 1e-6
+solver = proxddp.SolverProxDDP(TOL, mu_init, rho_init, 200, verbose=proxddp.VerboseLevel.VERBOSE)
+# solver.rol_type = proxddp.RolloutType.NONLINEAR
 solver.setup(problem)
 
 xs_init = [x0] * (nsteps + 1)
@@ -72,12 +87,27 @@ results = solver.getResults()
 print(results)
 
 
+pts = get_endpoint_traj(rmodel, rdata, results.xs, tool_id_lh)
+ax = plt.subplot(111, projection='3d')
+ax.plot(*pts.T, lw=1.0)
+ax.scatter(*target_ee_pos, marker='^', c="r")
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+ax.set_zlabel("$z$")
+plt.tight_layout()
+plt.show()
+
 vizer = pin.visualize.MeshcatVisualizer(
     rmodel, robot.collision_model, robot.visual_model, data=rdata
 )
-vizer.initViewer(loadModel=True)
+vizer.initViewer(open=True, loadModel=True)
+
+q0 = x0[: rmodel.nq]
+vizer.display(q0)
 
 vizutil = msu.VizUtil(vizer)
+vizutil.set_cam_pos([1.0, 0.0, 1.0])
+vizutil.set_cam_target([0.0, 0.0, 1.0])
 
 import time
 
