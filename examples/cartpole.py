2015bf8e32014f5af9c248eb1c49eb278684d030
ManifoldFR <wilson.jallet@polytechnique.org>
2022-10-04 19:16:28 +0200

    examples: refactor cartpole code to utils.py file
    * add function to make multi-pendulum

--- examples/cartpole.py
+++ examples/cartpole.py
@@ -9,10 +9,10 @@ from pinocchio.visualize import MeshcatVisualizer
 import numpy as np
 import proxddp
 import proxnlp
-import hppfcl as fcl
 import tap
 import matplotlib.pyplot as plt
 import meshcat_utils as msu
+from utils import create_cartpole
 
 
 class Args(tap.Tap):
@@ -26,90 +26,6 @@ class Args(tap.Tap):
 
 
 args = Args().parse_args()
-
-
-def create_cartpole(N):
-    model = pin.Model()
-    geom_model = pin.GeometryModel()
-
-    parent_id = 0
-
-    cart_radius = 0.1
-    cart_length = 5 * cart_radius
-    cart_mass = 1.0
-    joint_name = "joint_cart"
-
-    geometry_placement = pin.SE3.Identity()
-    geometry_placement.rotation = pin.Quaternion(
-        np.array([0.0, 0.0, 1.0]), np.array([0.0, 1.0, 0.0])
-    ).toRotationMatrix()
-
-    joint_id = model.addJoint(
-        parent_id, pin.JointModelPY(), pin.SE3.Identity(), joint_name
-    )
-
-    body_inertia = pin.Inertia.FromCylinder(cart_mass, cart_radius, cart_length)
-    body_placement = geometry_placement
-    model.appendBodyToJoint(
-        joint_id, body_inertia, body_placement
-    )  # We need to rotate the inertia as it is expressed in the LOCAL frame of the geometry
-
-    shape_cart = fcl.Cylinder(cart_radius, cart_length)
-
-    geom_cart = pin.GeometryObject(
-        "shape_cart", joint_id, geometry_placement, shape_cart
-    )
-    geom_cart.meshColor = np.array([1.0, 0.1, 0.1, 1.0])
-    geom_model.addGeometryObject(geom_cart)
-
-    parent_id = joint_id
-    joint_placement = pin.SE3.Identity()
-    body_mass = 0.1
-    body_radius = 0.1
-    for k in range(N):
-        joint_name = "joint_" + str(k + 1)
-        joint_id = model.addJoint(
-            parent_id, pin.JointModelRX(), joint_placement, joint_name
-        )
-
-        body_inertia = pin.Inertia.FromSphere(body_mass, body_radius)
-        body_placement = joint_placement.copy()
-        body_placement.translation[2] = 1.0
-        model.appendBodyToJoint(joint_id, body_inertia, body_placement)
-
-        geom1_name = "ball_" + str(k + 1)
-        shape1 = fcl.Sphere(body_radius)
-        geom1_obj = pin.GeometryObject(geom1_name, joint_id, body_placement, shape1)
-        geom1_obj.meshColor = np.ones((4))
-        geom_model.addGeometryObject(geom1_obj)
-
-        geom2_name = "bar_" + str(k + 1)
-        shape2 = fcl.Cylinder(body_radius / 4.0, body_placement.translation[2])
-        shape2_placement = body_placement.copy()
-        shape2_placement.translation[2] /= 2.0
-
-        geom2_obj = pin.GeometryObject(geom2_name, joint_id, shape2_placement, shape2)
-        geom2_obj.meshColor = np.array([0.0, 0.0, 0.0, 1.0])
-        geom_model.addGeometryObject(geom2_obj)
-
-        parent_id = joint_id
-        joint_placement = body_placement.copy()
-    end_frame = pin.Frame(
-        "end_effector_frame",
-        model.getJointId("joint_" + str(N)),
-        0,
-        body_placement,
-        pin.FrameType(3),
-    )
-    model.addFrame(end_frame)
-    geom_model.collision_pairs = []
-    model.qinit = np.zeros(model.nq)
-    model.qinit[1] = 0.0 * np.pi
-    model.qref = pin.neutral(model)
-    data = model.createData()
-    geom_data = geom_model.createData()
-    ddl = np.array([0])
-    return model, geom_model, data, geom_data, ddl
 
 
 model, geom_model, data, geom_data, ddl = create_cartpole(1)
