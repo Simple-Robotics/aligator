02420a1e241f8f433010b7089738fad54f252b67
ManifoldFR <wilson.jallet@polytechnique.org>
2022-11-17 12:01:24 +0100

    exceptions: Make custom RuntimeError class

--- include/proxddp/utils/exceptions.hpp
+++ include/proxddp/utils/exceptions.hpp
@@ -4,6 +4,15 @@
 #include <fmt/core.h>
 
 #define PROXDDP_RUNTIME_ERROR(msg)                                             \
-  throw std::runtime_error(fmt::format("{}({}): {}", __FILE__, __LINE__, msg))
+  throw proxddp::RuntimeError(                                                 \
+      fmt::format("{}({}): {}", __FILE__, __LINE__, msg))
 
-namespace proxddp {} // namespace proxddp
+namespace proxddp {
+
+class RuntimeError : public std::runtime_error {
+public:
+  explicit RuntimeError(const std::string &what = "")
+      : std::runtime_error(what) {}
+};
+
+} // namespace proxddp
