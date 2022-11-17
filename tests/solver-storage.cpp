58244b9c5dabdf24a01a2a1fc93fda2ea9f6df5f
ManifoldFR <wilson.jallet@polytechnique.org>
2022-11-17 15:24:37 +0100

    tests/solver-storage: update for new def of value_storage struct

--- tests/solver-storage.cpp
+++ tests/solver-storage.cpp
@@ -15,19 +15,15 @@ BOOST_AUTO_TEST_CASE(prox_storage) {
   auto vstore = value_store_t(NX);
   auto qstore = q_store_t(NX, NU, NX);
 
-  Eigen::Map<Eigen::VectorXd> map(vstore.storage.data(), vstore.storage.size());
-  map.setLinSpaced(0., (double)vstore.storage.size() - 1.);
   Eigen::Map<Eigen::VectorXd> map_q(qstore.storage.data(),
                                     qstore.storage.size());
   map_q.setLinSpaced(0., (double)qstore.storage.size() - 1.);
 
   BOOST_TEST_MESSAGE("Checking value function");
-  BOOST_CHECK_EQUAL(vstore.storage.cols(), NX + 1);
   BOOST_CHECK_EQUAL(vstore.Vx().cols(), 1);
   BOOST_CHECK_EQUAL(vstore.Vx().rows(), NX);
   BOOST_CHECK_EQUAL(vstore.Vxx().rows(), NX);
   BOOST_CHECK_EQUAL(vstore.Vxx().cols(), NX);
-  fmt::print("{} < store\n", vstore.storage);
   fmt::print("{} < Vx\n", vstore.Vx());
   fmt::print("{} < Vxx\n", vstore.Vxx());
 
