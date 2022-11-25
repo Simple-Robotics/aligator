8bd6e0e26c74531f466a01b2ae4edb0a15a3a61f
ManifoldFR <wilson.jallet@polytechnique.org>
2022-11-25 15:08:21 +0100

    scripts: enhance make_flamegraph.sh
    pass FLAMEGRAPH_DIR as an envar, otherwise try (my) default value

--- scripts/make_flamegraph.sh
+++ scripts/make_flamegraph.sh
@@ -1,15 +1,17 @@
+# Use the brendangregg/FlameGraph repo with perl scripts
 set -x
 
-
 cd build
-CMD=./examples/example-croc-talos-arm
-FILTER=prox
-REPS=10
-# CMD_OPTS="--benchmark_filter=$FILTER"
-CMD_OPTS="--benchmark_repetitions=$REPS"
 
-BUILD_DIR=$PWD
-FLAMEGRAPH_DIR=$HOME/git-repos/FlameGraph
+CMD=./examples/example-croc-talos-arm
+CMD_OPTS=""
+
+FLAMEGRAPH_DIR=${FLAMEGRAPH_DIR:-${HOME}/git-repos/FlameGraph}
+if [ ! -d $FLAMEGRAPH_DIR ]
+then
+    echo "Supplied FLAMEGRAPH_DIR=${FLAMEGRAPH_DIR} is not a directory"
+    return 1
+fi
 
 ST_COLL="$FLAMEGRAPH_DIR/stackcollapse-perf.pl"
 FLAMEGRAPH_CMD="$FLAMEGRAPH_DIR/flamegraph.pl"
@@ -19,4 +21,4 @@ FLAMEGRAPH_CMD="$FLAMEGRAPH_DIR/flamegraph.pl"
 perf record --call-graph dwarf $CMD $CMD_OPTS
 perf script | $ST_COLL > out.folded
 $FLAMEGRAPH_CMD out.folded > out.svg
-
+rm out.folded
