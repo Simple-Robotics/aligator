45894fd11cc6c42356d2a8e61c40cce70b349a7b
ManifoldFR <wilson.jallet@polytechnique.org>
2023-02-08 20:27:15 +0100

    bench-croc-talos-arm: fix number of threads to 1

--- examples/bench/croc-talos-arm.cpp
+++ examples/bench/croc-talos-arm.cpp
@@ -17,12 +17,14 @@ using proxddp::SolverProxDDP;
 
 constexpr double TOL = 1e-16;
 constexpr std::size_t maxiters = 10;
+constexpr std::size_t DEFAULT_NUM_THREADS = 1;
 
 const bool verbose = false;
 
 static void BM_croc_fddp(benchmark::State &state) {
   const std::size_t nsteps = (std::size_t)state.range(0);
   auto croc_problem = defineCrocoddylProblem(nsteps);
+  croc_problem->set_nthreads((int)DEFAULT_NUM_THREADS);
 
   std::vector<VectorXd> xs_i;
   std::vector<VectorXd> us_i;
@@ -47,6 +49,7 @@ static void BM_prox_fddp(benchmark::State &state) {
   const std::size_t nsteps = (std::size_t)state.range(0);
   auto croc_problem = defineCrocoddylProblem(nsteps);
   auto prob_wrap = proxddp::compat::croc::convertCrocoddylProblem(croc_problem);
+  prob_wrap.setNumThreads(DEFAULT_NUM_THREADS);
 
   std::vector<VectorXd> xs_i;
   std::vector<VectorXd> us_i;
@@ -68,6 +71,7 @@ template <LDLTChoice choice> static void BM_proxddp(benchmark::State &state) {
   using proxddp::LDLTChoice;
   auto croc_problem = defineCrocoddylProblem(nsteps);
   auto prob_wrap = proxddp::compat::croc::convertCrocoddylProblem(croc_problem);
+  prob_wrap.setNumThreads(DEFAULT_NUM_THREADS);
 
   std::vector<VectorXd> xs_i;
   std::vector<VectorXd> us_i;
@@ -98,7 +102,6 @@ int main(int argc, char **argv) {
         ->Arg(20)
         ->DenseRange(nmin, nmax, ns)
         ->Unit(unit)
-        ->ArgName("nsteps")
         ->Complexity()
         ->UseRealTime();
   };
