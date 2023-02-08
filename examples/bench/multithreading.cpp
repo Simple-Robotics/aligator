482bb7e0b44842c5753392560dae04ea685111a3
ManifoldFR <wilson.jallet@polytechnique.org>
2023-02-08 20:25:18 +0100

    examples/bench-multithread : add croco FDDP bench

--- examples/bench/multithreading.cpp
+++ examples/bench/multithreading.cpp
@@ -16,6 +16,25 @@ using T = double;
 constexpr T TOL = std::numeric_limits<T>::epsilon();
 constexpr std::size_t maxiters = 10;
 
+static void BM_croc_multithread(benchmark::State &state) {
+  const std::size_t nsteps = (std::size_t)state.range(0);
+  const std::size_t nthreads = (std::size_t)state.range(1);
+
+  std::vector<VectorXd> xs_i, us_i;
+
+  auto croc_problem = defineCrocoddylProblem(nsteps);
+  getInitialGuesses(croc_problem, xs_i, us_i);
+
+  (*croc_problem).set_nthreads((int)nthreads);
+
+  croc::SolverFDDP solver(croc_problem);
+  solver.set_th_stop(TOL * TOL * static_cast<T>(nsteps));
+
+  for (auto _ : state) {
+    solver.solve(xs_i, us_i, maxiters);
+  }
+}
+
 static void BM_prox_multithread(benchmark::State &state) {
   using proxddp::compat::croc::convertCrocoddylProblem;
   const std::size_t nsteps = (std::size_t)state.range(0);
@@ -28,7 +47,8 @@ static void BM_prox_multithread(benchmark::State &state) {
   auto problem = convertCrocoddylProblem(croc_problem);
   problem.setNumThreads(nthreads);
 
-  SolverProxDDP<T> solver(TOL, 0.01, 0., maxiters);
+  const T mu_init = 1e-6;
+  SolverProxDDP<T> solver(TOL, mu_init, 0., maxiters);
   solver.setup(problem);
 
   for (auto _ : state) {
@@ -58,7 +78,7 @@ static void BM_fddp_multithread(benchmark::State &state) {
 }
 
 static void custom_args(benchmark::internal::Benchmark *b) {
-  std::vector<long> nsteps = {20, 50, 100};
+  std::vector<long> nsteps = {25, 50, 100};
   std::vector<long> nthreads = {1, 2, 4, 6, 8};
   for (auto &ns : nsteps)
     for (auto &nt : nthreads)
@@ -68,6 +88,7 @@ static void custom_args(benchmark::internal::Benchmark *b) {
 
 BENCHMARK(BM_prox_multithread)->Apply(custom_args);
 BENCHMARK(BM_fddp_multithread)->Apply(custom_args);
+BENCHMARK(BM_croc_multithread)->Apply(custom_args);
 
 int main(int argc, char **argv) {
   benchmark::Initialize(&argc, argv);
