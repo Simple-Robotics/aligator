22766f29b26375ed70f9238c10f6100494358d33
ManifoldFR <wilson.jallet@polytechnique.org>
2023-02-17 18:42:34 +0100

    fddp/linesearch : restore if/else succession from original solver
    + more fixes

--- include/proxddp/fddp/linesearch.hpp
+++ include/proxddp/fddp/linesearch.hpp
@@ -12,36 +12,43 @@ namespace proxddp {
 /// conditions.
 /// @return an std::pair
 template <typename Scalar, typename F, typename M>
-PROXDDP_INLINE std::pair<Scalar, Scalar>
-fddp_goldstein_linesearch(F &&phi, M &&model, const Scalar phi0,
-                          const typename Linesearch<Scalar>::Options &ls_params,
-                          Scalar th_grad, Scalar &d1) {
-  Scalar th_accept_step_ = 0.1;
-  Scalar th_accept_neg_step_ = 2.0;
+PROXDDP_INLINE std::pair<Scalar, Scalar> fddp_goldstein_linesearch(
+    F &&phi, M &&model, const Scalar phi0,
+    const typename Linesearch<Scalar>::Options &ls_params, Scalar th_grad,
+    Scalar &d1, Scalar th_accept_step = 0.1, Scalar th_accept_neg_step = 2.0) {
   const Scalar beta = ls_params.contraction_min;
   Scalar atry = 1.;
   Scalar phitry = phi0;
+  Scalar dVreal, dVmodel;
+  constexpr Scalar eps = std::numeric_limits<Scalar>::epsilon();
 
   // backtrack until going under alpha_min
-  do {
+  while (true) {
+    if (atry < ls_params.alpha_min + eps) {
+      break;
+    }
     try {
       phitry = phi(atry);
     } catch (const ::proxddp::RuntimeError &) {
       atry *= beta;
       continue;
     }
-    Scalar dVreal = phitry - phi0;
-    Scalar dVmodel = model(atry) - phi0;
+    dVreal = phitry - phi0;
+    dVmodel = model(atry) - phi0;
     // check if descent direction
-    bool descent_ok = (dVmodel <= 0.);
-    descent_ok &= (-d1 < th_grad) || (dVreal <= th_accept_step_ * dVmodel);
-    // or accept small increase in cost;
-    bool ascent_ok = dVreal <= th_accept_neg_step_ * dVmodel;
-    if (descent_ok || ascent_ok) {
-      break;
+    if (dVmodel < 0.) {
+      if (std::abs(d1) < th_grad || dVreal <= th_accept_step * dVmodel) {
+        break;
+      }
+    } else {
+      // or accept small increase in cost;
+      if (dVreal <= th_accept_neg_step * dVmodel) {
+        break;
+      }
     }
     atry *= beta;
-  } while (atry >= ls_params.alpha_min);
+    atry = std::max(atry, ls_params.alpha_min);
+  }
   return {atry, phitry};
 }
 
