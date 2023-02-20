fdfe589177ff82af5ebe75c821e46e4c2741f9b6
ManifoldFR <wilson.jallet@polytechnique.org>
2023-02-20 15:11:26 +0100

    macros: add macros for cpp14, cpp17 features
    + add maybe_unused macro

--- include/proxddp/macros.hpp
+++ include/proxddp/macros.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#if __cplusplus >= 201703L
+#define PROXDDP_WITH_CPP_17
+#endif
+
+#if __cplusplus >= 201402L
+#define PROXDDP_WITH_CPP_14
+#endif
+
 #ifdef PROXDDP_EIGEN_CHECK_MALLOC
 #define PROXDDP_EIGEN_ALLOW_MALLOC(allowed)                                    \
   ::Eigen::internal::set_is_malloc_allowed(allowed)
@@ -13,3 +21,11 @@
 #define PROXDDP_NOMALLOC_END PROXDDP_EIGEN_ALLOW_MALLOC(true)
 
 #define PROXDDP_INLINE inline __attribute__((always_inline))
+
+#ifdef PROXDDP_WITH_CPP_17
+#define PROXDDP_MAYBE_UNUSED [[maybe_unused]]
+#elif defined(_MSC_VER) && !defined(__clang__)
+#define PROXDDP_MAYBE_UNUSED
+#else
+#define PROXDDP_MAYBE_UNUSED __attribute__((__unused__))
+#endif
