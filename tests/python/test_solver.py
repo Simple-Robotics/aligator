9d1761517c5115dc48d1b61fadb40e34eab94962
ManifoldFR <wilson.jallet@polytechnique.org>
2023-02-28 12:09:09 +0100

    tests: simplify test_no_node

--- tests/python/test_solver.py
+++ tests/python/test_solver.py
@@ -8,30 +8,22 @@
 import sys
 
 import example_robot_data as erd
+from proxddp.manifolds import MultibodyPhaseSpace
 import numpy as np
 import proxddp
 import pytest
-from proxddp.dynamics import IntegratorEuler, MultibodyFreeFwdDynamics
-from proxnlp.manifolds import MultibodyPhaseSpace
-
-robot = erd.load("ur5")
-rmodel = robot.model
-rdata = robot.data
-space = MultibodyPhaseSpace(rmodel)
-actuation_matrix = np.eye(rmodel.nv)
-nu = actuation_matrix.shape[1]
-ode_dynamics = MultibodyFreeFwdDynamics(space, actuation_matrix)
-x0 = space.rand()
-nsteps = 500
-nq = rmodel.nq
-dt = 0.01
-discrete_dyn = IntegratorEuler(ode_dynamics, dt)
 
 
 def test_no_node():
+    robot = erd.load("ur5")
+    rmodel = robot.model
+    space = MultibodyPhaseSpace(rmodel)
+    actuation_matrix = np.eye(rmodel.nv)
+    nu = actuation_matrix.shape[1]
+    x0 = space.rand()
     mu_init = 4e-2
     rho_init = 1e-2
-    verbose = proxddp.VerboseLevel.VERBOSE
+    verbose = proxddp.VERBOSE
     TOL = 1e-4
     MAX_ITER = 200
     terminal_cost = proxddp.CostStack(space.ndx, nu)
