608cb2b0df2d38964d74ef2c86e1c51a167644c0
ManifoldFR <wilson.jallet@polytechnique.org>
2023-03-06 19:59:54 +0100

    python/utils : make deprecation policies more expressive
    + allow choice of warning type as a template argument

--- python/utils/deprecation.hpp
+++ python/utils/deprecation.hpp
@@ -10,8 +10,27 @@ namespace proxddp {
 namespace python {
 namespace bp = boost::python;
 
+enum class DeprecationTypes { DEPRECATION, PENDING_DEPRECATION, FUTURE };
+
+namespace detail {
+
+constexpr auto deprtype_to_pyobj(DeprecationTypes dep) {
+  switch (dep) {
+  case DeprecationTypes::DEPRECATION:
+    return PyExc_DeprecationWarning;
+  case DeprecationTypes::PENDING_DEPRECATION:
+    return PyExc_PendingDeprecationWarning;
+  case DeprecationTypes::FUTURE:
+    return PyExc_FutureWarning;
+  }
+}
+
+} // namespace detail
+
 /// A Boost.Python policy which triggers a Python warning on precall.
-template <class Policy = bp::default_call_policies>
+template <class Policy = bp::default_call_policies,
+          DeprecationTypes deprecation_type =
+              DeprecationTypes::PENDING_DEPRECATION>
 struct deprecation_warning_policy : Policy {
 
   using result_converter = typename Policy::result_converter;
@@ -25,7 +44,8 @@ struct deprecation_warning_policy : Policy {
   const Policy *derived() const { return static_cast<const Policy *>(this); }
 
   template <class ArgPackage> bool precall(const ArgPackage &args) const {
-    PyErr_WarnEx(PyExc_UserWarning, m_what.c_str(), 1);
+    PyErr_WarnEx(detail::deprtype_to_pyobj(deprecation_type), m_what.c_str(),
+                 1);
     return derived()->precall(args);
   }
 
@@ -33,9 +53,24 @@ private:
   const std::string m_what;
 };
 
-template <class Policy = bp::default_call_policies>
-struct deprecated_member : deprecation_warning_policy<Policy> {
-  using Base = deprecation_warning_policy<Policy>;
+template <class Policy = bp::default_call_policies,
+          DeprecationTypes deprecation_type =
+              DeprecationTypes::PENDING_DEPRECATION>
+struct deprecated_function
+    : deprecation_warning_policy<Policy, deprecation_type> {
+  using Base = deprecation_warning_policy<Policy, deprecation_type>;
+  deprecated_function(const std::string &warning_msg =
+                          "This function has been marked as deprecated, and "
+                          "will be removed in the future.")
+      : Base(warning_msg) {}
+};
+
+template <class Policy = bp::default_call_policies,
+          DeprecationTypes deprecation_type =
+              DeprecationTypes::PENDING_DEPRECATION>
+struct deprecated_member
+    : deprecation_warning_policy<Policy, deprecation_type> {
+  using Base = deprecation_warning_policy<Policy, deprecation_type>;
   deprecated_member(const std::string &warning_msg =
                         "This attribute has been marked as deprecated, and "
                         "will be removed in the future.")
