382bee67f9a69a05acab38139dae88bb3cea2a64
ManifoldFR <wilson.jallet@polytechnique.org>
2023-03-16 18:48:06 +0100

    modelling : instantiate CostStackTpl template struct

---
+++ src/modelling/sum-of-costs.cpp
@@ -0,0 +1,8 @@
+#include "proxddp/modelling/sum-of-costs.hpp"
+
+namespace proxddp {
+
+template struct CostStackTpl<context::Scalar>;
+template struct CostStackDataTpl<context::Scalar>;
+
+} // namespace proxddp
