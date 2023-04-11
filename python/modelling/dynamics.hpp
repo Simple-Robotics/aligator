fd883aaca15205905cd0f5825cf313e64d81c07b
ManifoldFR <wilson.jallet@polytechnique.org>
2023-04-11 17:44:50 +0200

    python/modelling/dynamics.hpp: add docstrings, add createData() and default_createData() overrides

--- python/modelling/dynamics.hpp
+++ python/modelling/dynamics.hpp
@@ -7,12 +7,17 @@ namespace proxddp {
 namespace python {
 namespace internal {
 
-template <class ExplicitBase = ExplicitDynamicsModelTpl<context::Scalar>>
+/// Wrapper for ExplicitDynamicsModel which avoids redeclaring overrides for any
+/// child virtual class (e.g. integrator classes).
+/// @tparam ExplicitBase The derived virtual class that is being exposed.
+/// @sa PyStageFunction
+template <class ExplicitBase = context::ExplicitDynamics>
 struct PyExplicitDynamics : ExplicitBase, bp::wrapper<ExplicitBase> {
   using Scalar = context::Scalar;
   PROXNLP_DYNAMIC_TYPEDEFS(Scalar);
   // All functions in the interface take this type for output
   using BaseData = ExplicitDynamicsDataTpl<Scalar>;
+  using FunctionData = FunctionDataTpl<Scalar>;
 
   template <typename... Args>
   PyExplicitDynamics(Args &&...args) : ExplicitBase(args...) {}
@@ -25,6 +30,15 @@ struct PyExplicitDynamics : ExplicitBase, bp::wrapper<ExplicitBase> {
   virtual void dForward(const ConstVectorRef &x, const ConstVectorRef &u,
                         BaseData &data) const {
     PROXDDP_PYTHON_OVERRIDE_PURE(void, "dForward", x, u, data);
+  }
+
+  shared_ptr<FunctionData> createData() const {
+    PROXDDP_PYTHON_OVERRIDE(shared_ptr<FunctionData>, ExplicitBase,
+                            createData, );
+  }
+
+  shared_ptr<FunctionData> default_createData() const {
+    return ExplicitBase::createData();
   }
 };
 
