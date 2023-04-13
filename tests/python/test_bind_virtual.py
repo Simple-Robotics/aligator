389a28daa7b53ab14b9b16bb6b15a2ca36b6243f
ManifoldFR <wilson.jallet@polytechnique.org>
2023-04-13 18:02:54 +0200

    tests: update bind_virtual_factory
    + with solution to the test

--- tests/python/test_bind_virtual.py
+++ tests/python/test_bind_virtual.py
@@ -10,19 +10,22 @@ class ImplClass(bvf.MyVirtualClass):
     def createData(self):
         return ImplData(self)
 
-    # override MyVirtualClass::doSomething(data)
-    def doSomething(self, data: bvf.MyVirtualData):
+    # override MyVirtualClass::doSomethingPtr(shared_ptr data)
+    def doSomethingPtr(self, data: bvf.MyVirtualData):
         print("Hello from doSomething!")
         data.printAddress()
         assert isinstance(data, ImplData)
         print("Data value:", data.value)
         data.value += 1
 
+    # override MyVirtualClass::doSomethingPtr(data&)
     def doSomethingRef(self, data: bvf.MyVirtualData):
         print("Hello from doSomethingRef!")
         data.printAddress()
+        print(type(data))
         assert isinstance(data, ImplData)
         print("Data value:", data.value)
+        data.value += 1
 
 
 class ImplData(bvf.MyVirtualData):
@@ -40,16 +43,38 @@ def test_instantiate_child():
 def test_call_do_something_ptr():
     obj = ImplClass()
     print("Calling doSomething (by ptr)")
-    d1 = bvf.callDoSomething(obj)
+    d1 = bvf.callDoSomethingPtr(obj)
     print("Output data.value:", d1.value)
 
 
 def test_call_do_something_ref():
     obj = ImplClass()
     print("Ref variant:")
-    with pytest.raises(AssertionError) as e_info:  # error *must* be raised
-        bvf.callDoSomethingRef(obj)
-        print(e_info)
+    d2 = bvf.callDoSomethingRef(obj)
+    print(d2.value)
+    print("-----")
+
+
+def test_iden_fns():
+
+    obj = ImplClass()
+    d = obj.createData()
+    print(d, type(d))
+
+    # take and return const T&
+    d1 = bvf.iden_ref(d)
+    print(d1, type(d1))
+    assert isinstance(d1, ImplData)
+
+    # take a shared_ptr, return const T&
+    d2 = bvf.iden_shared(d)
+    assert isinstance(d2, ImplData)
+    print(d2, type(d2))
+
+    print("copy shared ptr -> py -> cpp")
+    d3 = bvf.copy_shared(d)
+    assert isinstance(d3, ImplData)
+    print(d3, type(d3))
 
 
 if __name__ == "__main__":
