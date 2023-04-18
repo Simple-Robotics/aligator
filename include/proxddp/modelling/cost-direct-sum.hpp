67d63571fef64a031ebd047fb143e1bacbc6bf68
ManifoldFR <wilson.jallet@polytechnique.org>
2023-04-18 23:37:52 +0200

    core: Finish implementing DirectSumCost

--- include/proxddp/modelling/cost-direct-sum.hpp
+++ include/proxddp/modelling/cost-direct-sum.hpp
@@ -26,21 +26,18 @@ template <typename _Scalar> struct DirectSumCostTpl : CostAbstractTpl<_Scalar> {
   shared_ptr<BaseData> createData() const override;
 
   void evaluate(const ConstVectorRef &x, const ConstVectorRef &u,
-                BaseData &data) const override {
-    CartesianProduct const *space = get_product_space();
-  }
+                BaseData &data) const override;
   void computeGradients(const ConstVectorRef &x, const ConstVectorRef &u,
-                        BaseData &data) const override {
-    auto space = get_product_space();
-  }
+                        BaseData &data) const override;
   void computeHessians(const ConstVectorRef &x, const ConstVectorRef &u,
-                       BaseData &data) const override {}
+                       BaseData &data) const override;
 
 private:
   using CartesianProduct = proxnlp::CartesianProductTpl<Scalar>;
   auto get_product_space() const {
     return static_cast<CartesianProduct const *>(this->space.get());
   }
+  static Data &data_cast(BaseData &data) { return static_cast<Data &>(data); }
 };
 
 template <typename Scalar> struct DirectSumCostTpl<Scalar>::Data : BaseData {
@@ -50,6 +47,72 @@ template <typename Scalar> struct DirectSumCostTpl<Scalar>::Data : BaseData {
       : BaseData(model.ndx(), model.nu), data1_(model.c1_->createData()),
         data2_(model.c2_->createData()) {}
 };
+
+template <typename Scalar>
+void DirectSumCostTpl<Scalar>::evaluate(const ConstVectorRef &x,
+                                        const ConstVectorRef &u,
+                                        BaseData &data) const {
+  CartesianProduct const *space = get_product_space();
+  Data &d = data_cast(data);
+  auto xs = space->split(x);
+  ConstVectorRef u1 = u.head(c1_->nu);
+  ConstVectorRef u2 = u.tail(c2_->nu);
+
+  c1_->evaluate(xs[0], u1, *d.data1_);
+  c2_->evaluate(xs[1], u2, *d.data2_);
+
+  d.value_ = d.data1_->value_ + d.data2_->value_;
+}
+
+template <typename Scalar>
+void DirectSumCostTpl<Scalar>::computeGradients(const ConstVectorRef &x,
+                                                const ConstVectorRef &u,
+                                                BaseData &data) const {
+  CartesianProduct const *space = get_product_space();
+  Data &d = data_cast(data);
+  auto xs = space->split(x);
+  ConstVectorRef u1 = u.head(c1_->nu);
+  ConstVectorRef u2 = u.tail(c2_->nu);
+
+  BaseData &d1 = *d.data1_;
+  BaseData &d2 = *d.data2_;
+
+  c1_->computeGradients(xs[0], u1, d1);
+  c2_->computeGradients(xs[1], u2, d2);
+
+  d.Lx_.head(c1_->ndx()) = d1.Lx_;
+  d.Lx_.tail(c2_->ndx()) = d2.Lx_;
+  d.Lu_.head(c1_->nu) = d1.Lu_;
+  d.Lu_.tail(c2_->nu) = d2.Lu_;
+}
+
+template <typename Scalar>
+void DirectSumCostTpl<Scalar>::computeHessians(const ConstVectorRef &x,
+                                               const ConstVectorRef &u,
+                                               BaseData &data) const {
+  CartesianProduct const *space = get_product_space();
+  Data &d = data_cast(data);
+  auto xs = space->split(x);
+  ConstVectorRef u1 = u.head(c1_->nu);
+  ConstVectorRef u2 = u.tail(c2_->nu);
+
+  BaseData &d1 = *d.data1_;
+  BaseData &d2 = *d.data2_;
+
+  c1_->computeHessians(xs[0], u1, d1);
+  c2_->computeHessians(xs[1], u2, d2);
+
+  d.Lxx_.topLeftCorner(c1_->ndx(), c1_->ndx()) = d1.Lxx_;
+  d.Lxx_.bottomRightCorner(c2_->ndx(), c2_->ndx()) = d2.Lxx_;
+
+  d.Luu_.topLeftCorner(c1_->nu, c1_->nu) = d1.Luu_;
+  d.Luu_.bottomRightCorner(c2_->nu, c2_->nu) = d2.Luu_;
+
+  d.Lxu_.topLeftCorner(c1_->ndx(), c1_->nu) = d1.Lxu_;
+  d.Lxu_.bottomRightCorner(c2_->ndx(), c2_->nu) = d2.Lxu_;
+
+  d.Lux_ = d.Lxu_.transpose();
+}
 
 template <typename Scalar>
 auto DirectSumCostTpl<Scalar>::createData() const -> shared_ptr<BaseData> {
--- python/modelling/expose-cost-ops.cpp
+++ python/modelling/expose-cost-ops.cpp
@@ -11,6 +11,7 @@ using context::Scalar;
 using DirectSumCost = DirectSumCostTpl<Scalar>;
 
 void exposeCostOps() {
+  bp::register_ptr_to_python<shared_ptr<DirectSumCost>>();
   bp::class_<DirectSumCost, bp::bases<CostBase>>("DirectSumCost", bp::no_init)
       .def(bp::init<shared_ptr<CostBase>, shared_ptr<CostBase>>(
           bp::args("self", "cost1", "cost2")))
@@ -21,6 +22,10 @@ void exposeCostOps() {
       "DirectSumCostData", bp::no_init)
       .def_readonly("data1", &DirectSumCost::Data::data1_)
       .def_readonly("data2", &DirectSumCost::Data::data2_);
+
+  bp::def("directSum", directSum<Scalar>, bp::args("cost1", "cost2"),
+          "Perform the direct sum of two cost functions, :math:`l_3(x,u) = "
+          "l_1(x_1,u_1) + l_2(x_2,u_2)`");
 }
 
 } // namespace python
