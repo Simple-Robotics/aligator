aca4fddac9a217ab96353d0b3f9e7a8cee704c35
ManifoldFR <wilson.jallet@polytechnique.org>
2023-04-24 12:20:29 +0200

    python: refactor expose-functions.cpp

--- python/expose-functions.cpp
+++ python/expose-functions.cpp
@@ -1,3 +1,4 @@
+/// @file
 /// @copyright Copyright (C) 2022 LAAS-CNRS, INRIA
 #include "proxddp/python/functions.hpp"
 #include "proxddp/python/eigen-member.hpp"
@@ -6,7 +7,6 @@
 #include "proxddp/modelling/linear-function.hpp"
 #include "proxddp/modelling/control-box-function.hpp"
 #include "proxddp/modelling/function-xpr-slice.hpp"
-#include "proxddp/modelling/linear-function-composition.hpp"
 
 namespace proxddp {
 namespace python {
@@ -167,52 +167,8 @@ void exposeFunctionBase() {
           bp::args("self", "ndx", "nu", "umin", "umax")));
 }
 
-void exposeFunctionExpressions() {
-  // FUNCTION SLICE
-
-  using FunctionSliceXpr = FunctionSliceXprTpl<Scalar>;
-
-  bp::class_<FunctionSliceXpr, bp::bases<StageFunction>>(
-      "FunctionSliceXpr",
-      "Represents a slice of an expression according to either a single index "
-      "or an array of indices.",
-      bp::init<FunctionPtr, std::vector<int>>(
-          bp::args("self", "func", "indices")))
-      .def(bp::init<FunctionPtr, const int>("Constructor from a single index.",
-                                            bp::args("self", "func", "idx")));
-
-  bp::class_<FunctionSliceXpr::Data, bp::bases<FunctionData>,
-             boost::noncopyable>("FunctionSliceData", bp::no_init)
-      .def_readonly("sub_data", &FunctionSliceXpr::Data::sub_data,
-                    "Underlying function's data.");
-
-  /// FUNCTION LINEAR COMPOSE
-
-  using LinearFunctionComposition = LinearFunctionCompositionTpl<Scalar>;
-
-  bp::class_<LinearFunctionComposition, bp::bases<StageFunction>>(
-      "LinearFunctionComposition",
-      "Function composition :math:`r(x) = Af(x) + b`.",
-      bp::init<FunctionPtr, const context::MatrixXs, const context::VectorXs>(
-          "Construct a composition from the underlying function, weight matrix "
-          ":math:`A` and bias :math:`b`.",
-          bp::args("self", "func", "A", "b")))
-      .def(bp::init<FunctionPtr, const context::MatrixXs>(
-          "Constructor where the bias :math:`b` is assumed to be zero.",
-          bp::args("self", "func", "A")))
-      .def_readonly("func", &LinearFunctionComposition::func,
-                    "The underlying function.")
-      .add_property("A",
-                    make_getter_eigen_matrix(&LinearFunctionComposition::A),
-                    "Weight matrix.")
-      .add_property("b",
-                    make_getter_eigen_matrix(&LinearFunctionComposition::A),
-                    "Bias vector.");
-
-  bp::class_<LinearFunctionComposition::OwnData, bp::bases<FunctionData>,
-             boost::noncopyable>("LinearFunctionCompositionData", bp::no_init)
-      .def_readonly("sub_data", &LinearFunctionComposition::OwnData::sub_data);
-}
+// fwd declaration
+void exposeFunctionExpressions();
 
 void exposeFunctions() {
   exposeFunctionBase();
