58c4501c7d428db997cf939658e0fc07a1bfce9d
ManifoldFR <wilson.jallet@polytechnique.org>
2023-06-28 17:45:32 +0200

    [python] move some utils from examples/utils into proxddp.utils

--- examples/utils.py
+++ examples/utils.py
@@ -8,8 +8,9 @@ import example_robot_data as erd
 import tap
 
 import matplotlib.pyplot as plt
+
+from proxddp.utils.plotting import *  # noqa
 from typing import Literal, List, Optional
-from proxddp import HistoryCallback, Results
 
 
 plt.rcParams["lines.linewidth"] = 1.0
@@ -17,19 +18,6 @@ plt.rcParams["lines.markersize"] = 5
 
 integrator_choices = Literal["euler", "semieuler", "midpoint", "rk2"]
 MESHCAT_ZMQ_DEFAULT = "tcp://127.0.0.1:6000"
-
-
-def plot_convergence(cb: HistoryCallback, ax: plt.Axes, res: Results = None):
-    from proxnlp.utils import plot_pd_errs
-
-    prim_infeas = cb.storage.prim_infeas.tolist()
-    dual_infeas = cb.storage.dual_infeas.tolist()
-    if res is not None:
-        prim_infeas.append(res.primal_infeas)
-        dual_infeas.append(res.dual_infeas)
-    plot_pd_errs(ax, prim_infeas, dual_infeas)
-    ax.grid(axis="y", which="major")
-    return
 
 
 class ArgsBase(tap.Tap):
@@ -45,20 +33,6 @@ class ArgsBase(tap.Tap):
             self.display = True
         if self.zmq_url is not None and self.zmq_url.lower() == "none":
             self.zmq_url = None
-
-
-def plot_se2_pose(q: np.ndarray, ax: plt.Axes, alpha=0.5, fc="tab:blue"):
-    from matplotlib import transforms
-
-    w = 1.0
-    h = 0.4
-    center = (q[0] - 0.5 * w, q[1] - 0.5 * h)
-    rect = plt.Rectangle(center, w, h, fc=fc, alpha=alpha)
-    theta = np.arctan2(q[3], q[2])
-    transform_ = transforms.Affine2D().rotate_around(*q[:2], -theta) + ax.transData
-    rect.set_transform(transform_)
-    ax.add_patch(rect)
-    return rect
 
 
 def get_endpoint(rmodel, rdata, q: np.ndarray, tool_id: int):
@@ -246,82 +220,3 @@ def add_namespace_prefix_to_models(model, collision_model, visual_model, namespa
     # Rename joints in model:
     for k in range(len(model.names)):
         model.names[k] = f"{namespace}/{model.names[k]}"
-
-
-def plot_controls_traj(
-    times,
-    us,
-    ncols=2,
-    axes=None,
-    effort_limit=None,
-    joint_names=None,
-    rmodel: pin.Model = None,
-):
-    t0 = times[0]
-    tf = times[-1]
-    us = np.asarray(us)
-    nu = us.shape[1]
-    nrows, r = divmod(nu, ncols)
-    nrows += int(r > 0)
-    if axes is None:
-        fig, axes = plt.subplots(nrows, ncols, sharex="col", figsize=(6.4, 6.4))
-    else:
-        fig = axes[0].get_figure()
-
-    if rmodel is not None:
-        effort_limit = rmodel.effortLimit
-        joint_names = rmodel.names
-
-    axes = axes.flatten()
-    for i in range(nu):
-        ax: plt.Axes = axes[i]
-        ax.step(times[:-1], us[:, i])
-        if effort_limit is not None:
-            ylim = ax.get_ylim()
-            ax.hlines(-effort_limit[i], t0, tf, colors="k", linestyles="--")
-            ax.hlines(+effort_limit[i], t0, tf, colors="r", linestyles="dashdot")
-            ax.set_ylim(*ylim)
-        if joint_names is not None:
-            joint_name = joint_names[i].lower()
-            ax.set_ylabel(joint_name)
-    fig.supxlabel("Time $t$")
-    fig.suptitle("Controls trajectory")
-    fig.tight_layout()
-    return fig
-
-
-def plot_velocity_traj(times, vs, rmodel: pin.Model, ncols=2):
-    vs = np.asarray(vs)
-    nv = vs.shape[1]
-    idx_to_joint_id_map = {}
-    jid = 0
-    for i in range(nv):
-        if i in rmodel.idx_vs.tolist():
-            jid += 1
-        idx_to_joint_id_map[i] = jid
-    print(idx_to_joint_id_map)
-    nrows, r = divmod(nv, ncols)
-    nrows += int(r > 0)
-
-    vel_limit = rmodel.velocityLimit
-    t0 = times[0]
-    tf = times[-1]
-
-    fig, axes = plt.subplots(nrows, ncols)
-    fig: plt.Figure
-    axes = axes.flatten()
-    for i in range(nv):
-        ax: plt.Axes = axes[i]
-        ax.plot(times, vs[:, i])
-        jid = idx_to_joint_id_map[i]
-        joint_name = rmodel.names[jid].lower()
-        ylim = ax.get_ylim()
-        ax.hlines(-vel_limit[i], t0, tf, colors="k", linestyles="--")
-        ax.hlines(+vel_limit[i], t0, tf, colors="r", linestyles="dashdot")
-        ax.set_ylim(*ylim)
-        ax.set_ylabel(joint_name)
-
-    fig.supxlabel("Time $t$")
-    fig.suptitle("Velocity trajectory")
-    fig.tight_layout()
-    return fig
