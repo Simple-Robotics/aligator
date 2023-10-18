14ff8536ad839e47e75468de9ebffd8169696b9c
Ludovic De Matteis <ldematteis@laas.fr>
2023-10-18 10:17:28 +0200

    Add center of mass velocity residual

--- python/modelling/expose-center-of-mass.cpp
+++ python/modelling/expose-center-of-mass.cpp
@@ -7,6 +7,7 @@
 
 #include "proxddp/python/modelling/multibody-utils.hpp"
 #include "proxddp/modelling/multibody/center-of-mass-translation.hpp"
+#include "proxddp/modelling/multibody/center-of-mass-velocity.hpp"
 
 namespace proxddp {
 namespace python {
@@ -20,6 +21,9 @@ using context::UnaryFunction;
 void exposeCenterOfMassFunctions() {
   using CenterOfMassTranslation = CenterOfMassTranslationResidualTpl<Scalar>;
   using CenterOfMassTranslationData = CenterOfMassTranslationDataTpl<Scalar>;
+
+  using CenterOfMassVelocity = CenterOfMassVelocityResidualTpl<Scalar>;
+  using CenterOfMassVelocityData = CenterOfMassVelocityDataTpl<Scalar>;
 
   bp::class_<CenterOfMassTranslation, bp::bases<UnaryFunction>>(
       "CenterOfMassTranslationResidual",
@@ -37,8 +41,28 @@ void exposeCenterOfMassFunctions() {
   bp::register_ptr_to_python<shared_ptr<CenterOfMassTranslationData>>();
 
   bp::class_<CenterOfMassTranslationData, bp::bases<FunctionData>>(
-      "FlyHighResidualData", "Data Structure for CenterOfMassTranslation",bp::no_init)
+      "CenterOfMassTranslationResidualData", "Data Structure for CenterOfMassTranslation",bp::no_init)
       .def_readonly("pin_data", &CenterOfMassTranslationData::pin_data_,
+                    "Pinocchio data struct.");
+
+  bp::class_<CenterOfMassVelocity, bp::bases<UnaryFunction>>(
+      "CenterOfMassVelocityResidual",
+      "A residual function :math:`r(x) = vcom(x)` ",
+      bp::init<const int, const int, shared_ptr<PinModel>, const context::Vector3s>(
+          bp::args("self", "ndx", "nu", "model", "v_ref")))
+      .def(FrameAPIVisitor<CenterOfMassVelocity>())
+      .def("getReference", &CenterOfMassVelocity::getReference, bp::args("self"),
+           bp::return_internal_reference<>(),
+           "Get the target Center Of Mass velocity.")
+      .def("setReference", &CenterOfMassVelocity::setReference,
+           bp::args("self", "p_new"),
+           "Set the target Center Of Mass velocity.");
+
+  bp::register_ptr_to_python<shared_ptr<CenterOfMassVelocityData>>();
+
+  bp::class_<CenterOfMassVelocityData, bp::bases<FunctionData>>(
+      "CenterOfMassVelocityResidualData", "Data Structure for CenterOfMassVelocity",bp::no_init)
+      .def_readonly("pin_data", &CenterOfMassVelocityData::pin_data_,
                     "Pinocchio data struct.");
 }
 
