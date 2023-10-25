26a7d56e0a91747fbfc68ad610b23778faaa88a7
ManifoldFR <wilson.jallet@polytechnique.org>
2023-10-25 15:37:41 +0200

    [tests] problem.cpp : remove header generate-problem.hpp

--- tests/problem.cpp
+++ tests/problem.cpp
@@ -1,15 +1,82 @@
 #include "aligator/core/traj-opt-problem.hpp"
 #include "aligator/solvers/proxddp/results.hpp"
 #include "aligator/solvers/proxddp/workspace.hpp"
+#include "aligator/core/explicit-dynamics.hpp"
 #include "aligator/utils/rollout.hpp"
-
-#include "generate-problem.hpp"
-#include <proxsuite-nlp/modelling/spaces/vector-space.hpp>
+#include "aligator/core/trajectory.hpp"
+#include "aligator/modelling/state-error.hpp"
+#include <proxsuite-nlp/modelling/spaces/pinocchio-groups.hpp>
 
 #include <boost/test/unit_test.hpp>
 
 #include <fmt/core.h>
 #include <fmt/ostream.h>
+
+using namespace aligator;
+
+/// @brief    Addition dynamics.
+/// @details  It maps \f$(x,u)\f$ to \f$ x + u \f$.
+struct MyModel : ExplicitDynamicsModelTpl<double> {
+  using Manifold = ManifoldAbstractTpl<double>;
+  using ExplicitData = ExplicitDynamicsDataTpl<double>;
+  explicit MyModel(const shared_ptr<Manifold> &space)
+      : ExplicitDynamicsModelTpl<double>(space, space->ndx()) {}
+
+  void forward(const ConstVectorRef &x, const ConstVectorRef &u,
+               ExplicitData &data) const {
+    space_next().integrate(x, u, data.xnext_);
+  }
+
+  void dForward(const ConstVectorRef &x, const ConstVectorRef &u,
+                ExplicitData &data) const {
+    space_next().Jintegrate(x, u, data.Jx_, 0);
+    space_next().Jintegrate(x, u, data.Ju_, 1);
+  }
+};
+
+struct MyCost : CostAbstractTpl<double> {
+  using CostAbstractTpl<double>::CostAbstractTpl;
+  void evaluate(const ConstVectorRef &, const ConstVectorRef &,
+                CostData &data) const {
+    data.value_ = 0.;
+  }
+
+  void computeGradients(const ConstVectorRef &, const ConstVectorRef &,
+                        CostData &data) const {
+    data.grad_.setZero();
+  }
+
+  void computeHessians(const ConstVectorRef &, const ConstVectorRef &,
+                       CostData &data) const {
+    data.hess_.setZero();
+  }
+};
+
+using Manifold = proxnlp::SETpl<3, double>;
+using StageModel = aligator::StageModelTpl<double>;
+using proxnlp::EqualityConstraint;
+
+struct MyFixture {
+  shared_ptr<Manifold> space;
+  const int nu;
+  const shared_ptr<MyModel> dyn_model;
+  const shared_ptr<MyCost> cost;
+  TrajOptProblemTpl<double> problem;
+
+  MyFixture()
+      : space(std::make_shared<Manifold>()), nu(space->ndx()),
+        dyn_model(std::make_shared<MyModel>(space)),
+        cost(std::make_shared<MyCost>(space, nu)),
+        problem(space->neutral(), nu, space, cost) {
+    auto stage = std::make_shared<StageModel>(cost, dyn_model);
+    auto func = std::make_shared<StateErrorResidualTpl<double>>(
+        space, nu, space->neutral());
+    auto stage2 = stage->clone();
+    stage2->addConstraint(func, std::make_shared<EqualityConstraint<double>>());
+    problem.addStage(stage);
+    problem.addStage(stage2);
+  }
+};
 
 BOOST_AUTO_TEST_SUITE(node)
 
@@ -20,7 +87,7 @@ BOOST_AUTO_TEST_CASE(test_problem) {
 
   auto nu = f.nu;
   auto &space = *f.space;
-  auto &stage = *f.stage;
+  auto &stage = *f.problem.stages_[0];
   BOOST_CHECK_EQUAL(stage.numPrimal(), space.ndx() + nu);
   BOOST_CHECK_EQUAL(stage.numDual(), space.ndx());
 
