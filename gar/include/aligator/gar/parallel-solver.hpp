fae2ad39534f0039c2db18c601106f07233072ed
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-27 17:17:11 +0100

    [gar/parallel-solver] add make_span_from_indices

--- gar/include/aligator/gar/parallel-solver.hpp
+++ gar/include/aligator/gar/parallel-solver.hpp
@@ -6,6 +6,18 @@
 namespace aligator {
 namespace gar {
 
+template <class T>
+boost::span<T> make_span_from_indices(std::vector<T> &vec, size_t i0,
+                                      size_t i1) {
+  return boost::make_span(vec.data() + i0, i1 - i0);
+}
+
+template <class T>
+boost::span<const T> make_span_from_indices(const std::vector<T> &vec,
+                                            size_t i0, size_t i1) {
+  return boost::make_span(vec.data() + i0, i1 - i0);
+}
+
 template <typename _Scalar> class ParallelRiccatiSolver {
 public:
   using Scalar = _Scalar;
