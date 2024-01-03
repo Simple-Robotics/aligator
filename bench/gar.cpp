a89ec741328cd6b448b8d6df516ece36400ce882
ManifoldFR <wilson.jallet@polytechnique.org>
2024-01-03 21:57:28 +0100

    [bench] gar.cpp : add missing include, test multiple processor numbers

--- bench/gar.cpp
+++ bench/gar.cpp
@@ -5,6 +5,7 @@
 
 #include "aligator/gar/riccati.hpp"
 #include "aligator/gar/parallel-solver.hpp"
+#include "aligator/gar/helpers.hpp"
 
 #include "../tests/gar/test_util.hpp"
 
@@ -28,11 +29,11 @@ static void BM_serial(benchmark::State &state) {
   }
 }
 
-static void BM_parallel(benchmark::State &state) {
+template <uint NPROC> static void BM_parallel(benchmark::State &state) {
   uint horz = (uint)state.range(0);
   VectorXs x0 = VectorXs::NullaryExpr(nx, normal_unary_op{});
   const LQProblem problem = generate_problem(x0, horz, nx, nu);
-  ParallelRiccatiSolver<double> solver(problem);
+  ParallelRiccatiSolver<double> solver(problem, NPROC);
   const double mu = 1e-11;
   auto [xs, us, vs, lbdas] = lqrInitializeSolution(problem);
   for (auto _ : state) {
@@ -42,7 +43,7 @@ static void BM_parallel(benchmark::State &state) {
 }
 
 static void customArgs(benchmark::internal::Benchmark *b) {
-  for (uint e = 3; e <= 7; e++) {
+  for (uint e = 4; e <= 8; e++) {
     b->Arg(1 << e);
   }
   b->Unit(benchmark::kMillisecond);
@@ -50,6 +51,8 @@ static void customArgs(benchmark::internal::Benchmark *b) {
 }
 
 BENCHMARK(BM_serial)->Apply(customArgs);
-BENCHMARK(BM_parallel)->Apply(customArgs);
+BENCHMARK_TEMPLATE(BM_parallel, 2)->Apply(customArgs);
+BENCHMARK_TEMPLATE(BM_parallel, 3)->Apply(customArgs);
+BENCHMARK_TEMPLATE(BM_parallel, 4)->Apply(customArgs);
 
 BENCHMARK_MAIN();
