75b1f4d44d4217b78a92d3abe5a1edda0dcc5114
ManifoldFR <wilson.jallet@polytechnique.org>
2024-01-04 12:17:25 +0100

    [bench] gar.cpp : remove useless typedef, test for NPROC=5 processors

--- bench/gar.cpp
+++ bench/gar.cpp
@@ -11,15 +11,13 @@
 
 using namespace aligator::gar;
 
-using LQProblem = LQRProblemTpl<double>;
-
-const uint nx = 8;
-const uint nu = 8;
+const uint nx = 36;
+const uint nu = 12;
 
 static void BM_serial(benchmark::State &state) {
   uint horz = (uint)state.range(0);
   VectorXs x0 = VectorXs::NullaryExpr(nx, normal_unary_op{});
-  const LQProblem problem = generate_problem(x0, horz, nx, nu);
+  const LQRProblemTpl<double> problem = generate_problem(x0, horz, nx, nu);
   ProximalRiccatiSolver<double> solver(problem);
   const double mu = 1e-11;
   auto [xs, us, vs, lbdas] = lqrInitializeSolution(problem);
@@ -32,7 +30,7 @@ static void BM_serial(benchmark::State &state) {
 template <uint NPROC> static void BM_parallel(benchmark::State &state) {
   uint horz = (uint)state.range(0);
   VectorXs x0 = VectorXs::NullaryExpr(nx, normal_unary_op{});
-  const LQProblem problem = generate_problem(x0, horz, nx, nu);
+  const LQRProblemTpl<double> problem = generate_problem(x0, horz, nx, nu);
   ParallelRiccatiSolver<double> solver(problem, NPROC);
   const double mu = 1e-11;
   auto [xs, us, vs, lbdas] = lqrInitializeSolution(problem);
@@ -54,5 +52,6 @@ BENCHMARK(BM_serial)->Apply(customArgs);
 BENCHMARK_TEMPLATE(BM_parallel, 2)->Apply(customArgs);
 BENCHMARK_TEMPLATE(BM_parallel, 3)->Apply(customArgs);
 BENCHMARK_TEMPLATE(BM_parallel, 4)->Apply(customArgs);
+BENCHMARK_TEMPLATE(BM_parallel, 5)->Apply(customArgs);
 
 BENCHMARK_MAIN();
