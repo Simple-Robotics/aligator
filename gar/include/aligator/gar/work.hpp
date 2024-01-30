c87bbc725173c284814206aed0205d68b3639231
ManifoldFR <wilson.jallet@polytechnique.org>
2024-01-31 00:43:25 +0100

    [gar] add header work.hpp

---
+++ gar/include/aligator/gar/work.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <assert.h>
+#include <sys/types.h>
+
+namespace aligator {
+namespace gar {
+
+struct workrange_t {
+  uint beg;
+  uint end;
+};
+
+inline workrange_t get_work(uint horz, uint tid, uint num_threads) {
+  uint start = tid * (horz + 1) / num_threads;
+  uint stop = (tid + 1) * (horz + 1) / num_threads;
+  assert(stop <= horz + 1);
+  return {start, stop};
+}
+
+} // namespace gar
+} // namespace aligator
