5507c187e3c3323bce317ce0daf78071c43371bd
Joris Vaillant <joris.vaillant@inria.fr>
2024-03-19 22:10:21 +0100

    common: Apply review

--- bindings/python/src/modelling/expose-constraintfwd.cpp
+++ bindings/python/src/modelling/expose-constraintfwd.cpp
@@ -43,9 +43,7 @@ void exposeConstrainedFwdDynamics() {
                      &MultibodyConstraintFwdDynamics::constraint_models_)
       .add_property("ntau", &MultibodyConstraintFwdDynamics::ntau,
                     "Torque dimension.")
-      .def(CreateDataPythonVisitor<
-           MultibodyConstraintFwdDynamics,
-           typename MultibodyConstraintFwdDynamics::BaseData>());
+      .def(CreateDataPythonVisitor<MultibodyConstraintFwdDynamics>());
 
   bp::register_ptr_to_python<shared_ptr<MultibodyConstraintFwdData>>();
 
