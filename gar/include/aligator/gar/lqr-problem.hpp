df1c62d371bfcf8004dcef4ebe47bb14b3e70b67
ManifoldFR <wilson.jallet@polytechnique.org>
2024-04-23 11:31:20 +0200

    [gar] add lqr-problem.hxx

--- gar/include/aligator/gar/lqr-problem.hpp
+++ gar/include/aligator/gar/lqr-problem.hpp
@@ -137,49 +137,6 @@ protected:
 };
 
 template <typename Scalar>
-Scalar LQRProblemTpl<Scalar>::evaluate(
-    const VectorOfVectors &xs, const VectorOfVectors &us,
-    const std::optional<ConstVectorRef> &theta_) const {
-  if ((int)xs.size() != horizon() + 1)
-    ALIGATOR_RUNTIME_ERROR(fmt::format(
-        "Wrong size for vector xs (expected {:d}).", horizon() + 1));
-  if ((int)us.size() < horizon())
-    ALIGATOR_RUNTIME_ERROR(
-        fmt::format("Wrong size for vector us (expected {:d}).", horizon()));
-
-  if (!isInitialized())
-    return 0.;
-
-  Scalar ret = 0.;
-  for (uint i = 0; i <= (uint)horizon(); i++) {
-    const LQRKnotTpl<Scalar> &knot = stages[i];
-    ret += 0.5 * xs[i].dot(knot.Q * xs[i]) + xs[i].dot(knot.q);
-    if (i == (uint)horizon())
-      break;
-    ret += 0.5 * us[i].dot(knot.R * us[i]) + us[i].dot(knot.r);
-    ret += xs[i].dot(knot.S * us[i]);
-  }
-
-  if (!isParameterized())
-    return ret;
-
-  if (theta_.has_value()) {
-    ConstVectorRef th = theta_.value();
-    for (uint i = 0; i <= (uint)horizon(); i++) {
-      const LQRKnotTpl<Scalar> &knot = stages[i];
-      ret += 0.5 * th.dot(knot.Gth * th);
-      ret += th.dot(knot.Gx.transpose() * xs[i]);
-      ret += th.dot(knot.gamma);
-      if (i == (uint)horizon())
-        break;
-      ret += th.dot(knot.Gu.transpose() * us[i]);
-    }
-  }
-
-  return ret;
-}
-
-template <typename Scalar>
 std::ostream &operator<<(std::ostream &oss, const LQRKnotTpl<Scalar> &self) {
   oss << "LQRKnot {";
   oss << fmt::format("\n  nx:  {:d}", self.nx) //
@@ -217,6 +174,8 @@ std::ostream &operator<<(std::ostream &oss, const LQRKnotTpl<Scalar> &self) {
 } // namespace gar
 } // namespace aligator
 
+#include "lqr-problem.hxx"
+
 #ifdef ALIGATOR_ENABLE_TEMPLATE_INSTANTIATION
-#include "./lqr-problem.txx"
+#include "lqr-problem.txx"
 #endif
