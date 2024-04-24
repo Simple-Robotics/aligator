383e1b2d7b7afb2166b18bcbb18bc97413ce0c81
ManifoldFR <wilson.jallet@polytechnique.org>
2024-04-24 22:33:23 +0200

    [tests/gar] block-matrix.cpp : use fixture

--- tests/gar/block-matrix.cpp
+++ tests/gar/block-matrix.cpp
@@ -49,32 +49,40 @@ BOOST_AUTO_TEST_CASE(dynamicblkvec) {
   BOOST_CHECK_EQUAL(bvtop2.rows(), 7);
 }
 
-BOOST_AUTO_TEST_CASE(block_tridiag_solve_up_looking) {
+struct BTAG_Fixture {
   size_t N = 6;
   uint nx = 2;
   MatrixXs B = MatrixXs::NullaryExpr(nx, nx, normal_unary_op{});
 
-  std::vector<MatrixXs> diagonal(N + 1);
-  std::vector<MatrixXs> sup(N);
-  std::vector<MatrixXs> sub(N);
-
-  for (size_t i = 0; i <= N; i++) {
-    diagonal[i] = sampleWishartDistributedMatrix(nx, nx + 1);
-  }
-
-  std::fill_n(sup.begin(), N, B);
-  std::fill_n(sub.begin(), N, B.transpose());
-
+  std::vector<MatrixXs> diagonal;
+  std::vector<MatrixXs> sup;
+  std::vector<MatrixXs> sub;
   using BlkVec = BlkMatrix<VectorXs, -1, 1>;
-  std::vector<long> dims(N + 1);
-  std::fill_n(dims.begin(), N + 1, nx);
-  BOOST_CHECK(dims.size() == N + 1);
-  BlkVec vec(dims);
-  vec.matrix().setOnes();
+  BlkVec vec;
+  MatrixXs densemat;
+  BlkVec rhs;
 
-  MatrixXs densemat = gar::blockTridiagToDenseMatrix(sub, diagonal, sup);
-  fmt::print("Dense problem matrix:\n{}\n", densemat);
-  BlkVec densevec = vec;
+  BTAG_Fixture() : diagonal(N + 1), sup(N), sub(N) {
+    for (size_t i = 0; i <= N; i++) {
+      diagonal[i] = sampleWishartDistributedMatrix(nx, nx + 1);
+    }
+
+    std::fill_n(sup.begin(), N, B);
+    std::fill_n(sub.begin(), N, B.transpose());
+
+    std::vector<long> dims(N + 1);
+    std::fill_n(dims.begin(), N + 1, nx);
+    BOOST_CHECK(dims.size() == N + 1);
+    vec = BlkVec(dims);
+    vec.matrix().setOnes();
+
+    densemat = gar::blockTridiagToDenseMatrix(sub, diagonal, sup);
+    fmt::print("Dense problem matrix:\n{}\n", densemat);
+    rhs = vec;
+  }
+};
+
+BOOST_FIXTURE_TEST_CASE(block_tridiag_solve_up_looking, BTAG_Fixture) {
 
   std::vector<Eigen::LDLT<MatrixXs>> facs(diagonal.size());
   bool ret = gar::symmetricBlockTridiagSolve(sub, diagonal, sup, vec, facs);
@@ -87,44 +95,17 @@ BOOST_AUTO_TEST_CASE(block_tridiag_solve_up_looking) {
   {
     // alternative solve
     Eigen::LDLT<MatrixXs> ldlt(densemat);
-    ldlt.solveInPlace(densevec.matrix());
+    ldlt.solveInPlace(rhs.matrix());
 
     fmt::print("Alternative solve:\n");
     for (size_t i = 0; i <= N; i++) {
-      fmt::print("rhs[{:d}] = {}\n", i, densevec[i].transpose());
+      fmt::print("rhs[{:d}] = {}\n", i, rhs[i].transpose());
     }
-    BOOST_CHECK(vec.matrix().isApprox(densevec.matrix(), 1e-12));
+    BOOST_CHECK(vec.matrix().isApprox(rhs.matrix(), 1e-12));
   }
 }
 
-BOOST_AUTO_TEST_CASE(block_tridiag_solve_down_looking) {
-  auto test_case_name = utf::current_test_case().full_name();
-  BOOST_TEST_MESSAGE(test_case_name);
-  size_t N = 6;
-  uint nx = 2;
-  MatrixXs B = MatrixXs::NullaryExpr(nx, nx, normal_unary_op{});
-
-  std::vector<MatrixXs> diagonal(N + 1);
-  std::vector<MatrixXs> sup(N);
-  std::vector<MatrixXs> sub(N);
-
-  for (size_t i = 0; i <= N; i++) {
-    diagonal[i] = sampleWishartDistributedMatrix(nx, nx + 1);
-  }
-
-  std::fill_n(sup.begin(), N, B);
-  std::fill_n(sub.begin(), N, B.transpose());
-
-  using BlkVec = BlkMatrix<VectorXs, -1, 1>;
-  std::vector<long> dims(N + 1);
-  std::fill_n(dims.begin(), N + 1, nx);
-  BOOST_CHECK(dims.size() == N + 1);
-  BlkVec vec(dims);
-  vec.matrix().setOnes();
-
-  MatrixXs densemat = gar::blockTridiagToDenseMatrix(sub, diagonal, sup);
-  fmt::print("Dense problem matrix:\n{}\n", densemat);
-  BlkVec densevec = vec;
+BOOST_FIXTURE_TEST_CASE(block_tridiag_solve_down_looking, BTAG_Fixture) {
 
   std::vector<Eigen::LDLT<MatrixXs>> facs(diagonal.size());
   bool ret =
@@ -138,12 +119,12 @@ BOOST_AUTO_TEST_CASE(block_tridiag_solve_down_looking) {
   {
     // alternative solve
     Eigen::LDLT<MatrixXs> ldlt(densemat);
-    ldlt.solveInPlace(densevec.matrix());
+    ldlt.solveInPlace(rhs.matrix());
 
     fmt::print("Alternative solve:\n");
     for (size_t i = 0; i <= N; i++) {
-      fmt::print("rhs[{:d}] = {}\n", i, densevec[i].transpose());
+      fmt::print("rhs[{:d}] = {}\n", i, rhs[i].transpose());
     }
-    BOOST_CHECK(vec.matrix().isApprox(densevec.matrix(), 1e-12));
+    BOOST_CHECK(vec.matrix().isApprox(rhs.matrix(), 1e-12));
   }
 }
