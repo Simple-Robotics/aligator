aebe6462402be82d9821d39d099e24bd7e1c4e33
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-16 23:52:30 +0200

    [cmake] do not compile test-cpp-nomalloc if CHECK_RUNTIME_MALLOC option is OFF

--- tests/nomalloc.cpp
+++ tests/nomalloc.cpp
@@ -6,18 +6,15 @@
 #include <aligator/eigen-macros.hpp>
 
 BOOST_AUTO_TEST_CASE(begin_end_basic) {
-#ifdef ALIGATOR_EIGEN_CHECK_MALLOC
   ALIGATOR_NOMALLOC_BEGIN;
   BOOST_CHECK(!Eigen::internal::is_malloc_allowed());
   ALIGATOR_NOMALLOC_END;
 
   // check if malloc legal
   BOOST_CHECK(Eigen::internal::is_malloc_allowed());
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(scoped) {
-#ifdef ALIGATOR_EIGEN_CHECK_MALLOC
   {
     ALIGATOR_NOMALLOC_SCOPED;
     BOOST_CHECK(!Eigen::internal::is_malloc_allowed());
@@ -25,11 +22,9 @@ BOOST_AUTO_TEST_CASE(scoped) {
 
   BOOST_CHECK(aligator::internal::get_cached_malloc_status());
   BOOST_CHECK(Eigen::internal::is_malloc_allowed());
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(nested) {
-#ifdef ALIGATOR_EIGEN_CHECK_MALLOC
   ALIGATOR_NOMALLOC_BEGIN;
   {
     ALIGATOR_NOMALLOC_SCOPED;
@@ -47,5 +42,4 @@ BOOST_AUTO_TEST_CASE(nested) {
   BOOST_CHECK(!Eigen::internal::is_malloc_allowed());
   ALIGATOR_NOMALLOC_END;
   BOOST_CHECK(Eigen::internal::is_malloc_allowed());
-#endif
 }
