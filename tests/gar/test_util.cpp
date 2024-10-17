30f85b46c04c16c48079055af970c4cc0f188de1
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-17 10:58:55 +0200

    [tests/gar] add knot.cpp test
    + refactor test_util : expose generate_knot()

--- tests/gar/test_util.cpp
+++ tests/gar/test_util.cpp
@@ -10,53 +10,50 @@ MatrixXs sampleWishartDistributedMatrix(uint n, uint p) {
   return root * root.transpose();
 };
 
+knot_t generate_knot(uint nx, uint nu, uint nth, bool singular) {
+  uint wishartDof = nx + nu + 1;
+  knot_t out(nx, nu, 0, nx, nth);
+
+  MatrixXs _qsr = sampleWishartDistributedMatrix(nx + nu, wishartDof);
+
+  out.Q = _qsr.topLeftCorner(nx, nx);
+  out.S = _qsr.topRightCorner(nx, nu);
+  if (singular) {
+    auto n = nx / 2;
+    out.Q.topLeftCorner(n, n).setZero();
+  }
+  out.R = _qsr.bottomRightCorner(nu, nu);
+  out.q = VectorXs::NullaryExpr(nx, normal_unary_op{});
+  out.r = VectorXs::NullaryExpr(nu, normal_unary_op{});
+
+  // out.A = MatrixXs::NullaryExpr(nx, nx, normal_unary_op{});
+  // out.B.setRandom();
+  // out.E = out.E.NullaryExpr(nx, nx, normal_unary_op{});
+  // out.E *= 1000;
+  // out.f = VectorXs::NullaryExpr(nx, normal_unary_op{});
+
+  // if (nth > 0) {
+  //   out.Gx = MatrixXs::NullaryExpr(nx, nth, normal_unary_op{});
+  //   out.Gu = MatrixXs::NullaryExpr(nu, nth, normal_unary_op{});
+  //   out.Gth = sampleWishartDistributedMatrix(nth, nth + 2);
+  //   out.gamma = VectorXs::NullaryExpr(nth, normal_unary_op{});
+  // }
+
+  return out;
+}
+
 problem_t generate_problem(const ConstVectorRef &x0, uint horz, uint nx,
                            uint nu, uint nth) {
   assert(x0.size() == nx);
 
   problem_t::KnotVector knots;
   knots.reserve(horz + 1);
-  uint wishartDof = nx + nu + 1;
 
-  auto gen = [&](uint nu, bool sing = false) {
-    knot_t out(nx, nu, 0);
-    out.addParameterization(nth);
-
-    MatrixXs _qsr = sampleWishartDistributedMatrix(nx + nu, wishartDof);
-
-    out.Q = _qsr.topLeftCorner(nx, nx);
-    // out.S = _qsr.topRightCorner(nx, nu);
-    if (sing) {
-      auto n = nx / 2;
-      if (nx <= 6)
-        n = nx / 2;
-      out.Q.topLeftCorner(n, n).setZero();
-    }
-    out.R = _qsr.bottomRightCorner(nu, nu);
-    out.q.head(nx) = VectorXs::NullaryExpr(nx, normal_unary_op{});
-    out.r.head(nu) = VectorXs::NullaryExpr(nu, normal_unary_op{});
-
-    out.A = MatrixXs::NullaryExpr(nx, nx, normal_unary_op{});
-    out.B.setRandom();
-    out.E = out.E.NullaryExpr(nx, nx, normal_unary_op{});
-    out.E *= 1000;
-    // out.E.setIdentity();
-    // out.E *= -1;
-    out.f.head(nx) = VectorXs::NullaryExpr(nx, normal_unary_op{});
-
-    out.Gx = MatrixXs::NullaryExpr(nx, nth, normal_unary_op{});
-    out.Gu = MatrixXs::NullaryExpr(nu, nth, normal_unary_op{});
-    out.Gth = sampleWishartDistributedMatrix(nth, nth + 2);
-    out.gamma = VectorXs::NullaryExpr(nth, normal_unary_op{});
-
-    return out;
-  };
-
-  auto knb = gen(nu, true);
+  auto knb = generate_knot(nx, nu, nth, true);
   for (uint i = 0; i < horz; i++) {
     knots.push_back(knb);
   }
-  knots.push_back(gen(0)); // terminal node
+  knots.push_back(generate_knot(nx, 0, nth)); // terminal node
 
   problem_t prob(std::move(knots), nx);
   prob.g0 = -x0;
