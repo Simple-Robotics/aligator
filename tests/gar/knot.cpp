c9900b18d3e04f196e72e15fcbb56aeaaaf8d905
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-17 10:58:55 +0200

    [tests/gar] add knot.cpp test
    + refactor test_util : expose generate_knot()

---
+++ tests/gar/knot.cpp
@@ -0,0 +1,94 @@
+#include "aligator/gar/lqr-problem.hpp"
+#include <proxsuite-nlp/fmt-eigen.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include "test_util.hpp"
+
+using namespace aligator::gar;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+struct knot_fixture {
+  uint nx = 2;
+  uint nu = 2;
+  knot_t knot;
+  knot_fixture() : knot(nx, nu, 0) {
+    knot.Q.setRandom();
+    knot.R.setRandom();
+    knot.q.setRandom();
+    knot.r.setRandom();
+
+    knot.A.setZero();
+    knot.B.setZero();
+    knot.E.setZero();
+    knot.f.setZero();
+  }
+};
+
+BOOST_FIXTURE_TEST_CASE(move, knot_fixture) {
+
+  MatrixXd Q = knot.Q;
+  MatrixXd R = knot.R;
+
+  knot_t knot_moved{std::move(knot)};
+  BOOST_CHECK_EQUAL(knot_moved.nx, nx);
+  BOOST_CHECK_EQUAL(knot_moved.nu, nu);
+  BOOST_CHECK(Q == knot_moved.Q);
+  BOOST_CHECK(R == knot_moved.R);
+
+  // copy ctor
+  knot_t knot_move2 = std::move(knot_moved);
+}
+
+BOOST_FIXTURE_TEST_CASE(copy, knot_fixture) {
+
+  knot_t knot2{knot};
+  BOOST_CHECK_EQUAL(knot.Q, knot2.Q);
+  BOOST_CHECK_EQUAL(knot.S, knot2.S);
+  BOOST_CHECK_EQUAL(knot.R, knot2.R);
+  BOOST_CHECK_EQUAL(knot.q, knot2.q);
+  BOOST_CHECK_EQUAL(knot.r, knot2.r);
+
+  BOOST_CHECK_EQUAL(knot.A, knot2.A);
+  BOOST_CHECK_EQUAL(knot.B, knot2.B);
+  BOOST_CHECK_EQUAL(knot.E, knot2.E);
+  BOOST_CHECK_EQUAL(knot.f, knot2.f);
+}
+
+BOOST_FIXTURE_TEST_CASE(swap, knot_fixture) {
+  using std::swap;
+  knot_t knot2 = knot;
+  knot2.Q.setIdentity();
+
+  fmt::println("knot.Q:\n{}", knot.Q);
+  fmt::println("knot2.Q:\n{}", knot2.Q);
+
+  swap(knot, knot2);
+
+  fmt::println("knot2.Q:\n{}", knot2.Q);
+}
+
+BOOST_FIXTURE_TEST_CASE(gen_knot, knot_fixture) {
+  knot_t knot2 = generate_knot(nx, nu, 0);
+  this->knot = knot2;
+}
+
+BOOST_AUTO_TEST_CASE(knot_vec) {
+  uint nx = 4;
+  uint nu = 2;
+  std::vector<knot_t> v;
+  v.reserve(10);
+  for (int i = 0; i < 10; i++) {
+    v.push_back(generate_knot(nx, nu, 0));
+  }
+
+  for (size_t i = 0; i < 10; i++) {
+    fmt::println("v [{:d}].q = {}", i, v[i].q.transpose());
+  }
+
+  std::vector<knot_t> v2 = std::move(v);
+  for (size_t i = 0; i < 10; i++) {
+    fmt::println("v2[{:d}].q = {}", i, v2[i].q.transpose());
+  }
+}
