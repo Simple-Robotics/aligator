f1b5a0f580b370ce1279b14f65f0536f76586ec6
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-26 11:56:25 +0200

    lagrangian.hpp : move def out-of-line

--- include/aligator/core/lagrangian.hpp
+++ include/aligator/core/lagrangian.hpp
@@ -19,57 +19,65 @@ template <typename Scalar> struct LagrangianDerivatives {
   static void compute(const TrajOptProblem &problem, const TrajOptData &pd,
                       const std::vector<VectorXs> &lams,
                       const std::vector<VectorXs> &vs,
-                      std::vector<VectorXs> &Lxs, std::vector<VectorXs> &Lus) {
-    using ConstraintStack = ConstraintStackTpl<Scalar>;
-    using StageFunctionData = StageFunctionDataTpl<Scalar>;
-    using DynamicsData = DynamicsDataTpl<Scalar>;
-    using CostData = CostDataAbstractTpl<Scalar>;
-    using StageModel = StageModelTpl<Scalar>;
-    using StageData = StageDataTpl<Scalar>;
-    const std::size_t nsteps = problem.numSteps();
+                      std::vector<VectorXs> &Lxs, std::vector<VectorXs> &Lus);
+};
 
-    ALIGATOR_TRACY_ZONE_SCOPED_N("LagrangianDerivatives::compute");
-    ALIGATOR_NOMALLOC_SCOPED;
+template <typename Scalar>
+void LagrangianDerivatives<Scalar>::compute(const TrajOptProblem &problem,
+                                            const TrajOptData &pd,
+                                            const std::vector<VectorXs> &lams,
+                                            const std::vector<VectorXs> &vs,
+                                            std::vector<VectorXs> &Lxs,
+                                            std::vector<VectorXs> &Lus) {
+  using ConstraintStack = ConstraintStackTpl<Scalar>;
+  using StageFunctionData = StageFunctionDataTpl<Scalar>;
+  using DynamicsData = DynamicsDataTpl<Scalar>;
+  using CostData = CostDataAbstractTpl<Scalar>;
+  using StageModel = StageModelTpl<Scalar>;
+  using StageData = StageDataTpl<Scalar>;
+  const std::size_t nsteps = problem.numSteps();
 
-    math::setZero(Lxs);
-    math::setZero(Lus);
+  ALIGATOR_TRACY_ZONE_SCOPED_N("LagrangianDerivatives::compute");
+  ALIGATOR_NOMALLOC_SCOPED;
 
-    // initial condition
-    const StageFunctionData &init_cond = *pd.init_data;
-    Lxs[0].noalias() = init_cond.Jx_.transpose() * lams[0];
+  math::setZero(Lxs);
+  math::setZero(Lus);
 
-    for (std::size_t i = 0; i < nsteps; i++) {
-      const StageModel &sm = *problem.stages_[i];
-      const StageData &sd = *pd.stage_data[i];
-      const ConstraintStack &stack = sm.constraints_;
-      const DynamicsData &dd = *sd.dynamics_data;
-      Lxs[i].noalias() +=
-          sd.cost_data->Lx_ + dd.Jx_.transpose() * lams[i + 1]; // [1] eqn. 24c
-      Lus[i].noalias() =
-          sd.cost_data->Lu_ + dd.Ju_.transpose() * lams[i + 1]; // [1] eqn. 24b
+  // initial condition
+  const StageFunctionData &init_cond = *pd.init_data;
+  Lxs[0].noalias() = init_cond.Jx_.transpose() * lams[0];
 
-      BlkView v_(vs[i], stack.dims());
-      for (std::size_t j = 0; j < stack.size(); j++) {
-        const StageFunctionData &cd = *sd.constraint_data[j];
-        Lxs[i].noalias() += cd.Jx_.transpose() * v_[j]; // [1] eqn. 24c
-        Lus[i].noalias() += cd.Ju_.transpose() * v_[j]; // [1] eqn. 24b
-      }
+  for (std::size_t i = 0; i < nsteps; i++) {
+    const StageModel &sm = *problem.stages_[i];
+    const StageData &sd = *pd.stage_data[i];
+    const ConstraintStack &stack = sm.constraints_;
+    const DynamicsData &dd = *sd.dynamics_data;
+    Lxs[i].noalias() +=
+        sd.cost_data->Lx_ + dd.Jx_.transpose() * lams[i + 1]; // [1] eqn. 24c
+    Lus[i].noalias() =
+        sd.cost_data->Lu_ + dd.Ju_.transpose() * lams[i + 1]; // [1] eqn. 24b
 
-      Lxs[i + 1].noalias() = dd.Jy_.transpose() * lams[i + 1]; // [1] eqn. 24b
+    BlkView v_(vs[i], stack.dims());
+    for (std::size_t j = 0; j < stack.size(); j++) {
+      const StageFunctionData &cd = *sd.constraint_data[j];
+      Lxs[i].noalias() += cd.Jx_.transpose() * v_[j]; // [1] eqn. 24c
+      Lus[i].noalias() += cd.Ju_.transpose() * v_[j]; // [1] eqn. 24b
     }
 
-    // terminal node
-    {
-      const CostData &cdterm = *pd.term_cost_data;
-      Lxs[nsteps] += cdterm.Lx_;
-      const ConstraintStack &stack = problem.term_cstrs_;
-      BlkView vN(vs[nsteps], stack.dims());
-      for (std::size_t j = 0; j < stack.size(); j++) {
-        const StageFunctionData &cd = *pd.term_cstr_data[j];
-        Lxs[nsteps].noalias() += cd.Jx_.transpose() * vN[j];
-      }
+    Lxs[i + 1].noalias() = dd.Jy_.transpose() * lams[i + 1]; // [1] eqn. 24b
+  }
+
+  // terminal node
+  {
+    const CostData &cdterm = *pd.term_cost_data;
+    Lxs[nsteps] += cdterm.Lx_;
+    const ConstraintStack &stack = problem.term_cstrs_;
+    BlkView vN(vs[nsteps], stack.dims());
+    for (std::size_t j = 0; j < stack.size(); j++) {
+      const StageFunctionData &cd = *pd.term_cstr_data[j];
+      Lxs[nsteps].noalias() += cd.Jx_.transpose() * vN[j];
     }
   }
-};
+}
 
 } // namespace aligator
