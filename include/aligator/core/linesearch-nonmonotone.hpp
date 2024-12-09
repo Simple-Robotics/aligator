5cc8a6bc6725d49ee0611040932a12b928a7a54d
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-09 15:45:56 +0100

    linesearch-nonmonotone.hpp : change template parameter name to "Scalar"

--- include/aligator/core/linesearch-nonmonotone.hpp
+++ include/aligator/core/linesearch-nonmonotone.hpp
@@ -10,34 +10,35 @@ using proxsuite::nlp::LinesearchStrategy;
 /// @brief Nonmonotone Linesearch algorithm. Modifies the Armijo condition with
 /// a moving average of function values.
 /// @details This is the algorithm from Zhang and Hager, SiOpt 2004.
-template <typename T> struct NonmonotoneLinesearch : Linesearch<T> {
-  using typename Linesearch<T>::FunctionSample;
-  using typename Linesearch<T>::Options;
-  using fun_t = std::function<T(T)>;
+template <typename Scalar> struct NonmonotoneLinesearch : Linesearch<Scalar> {
+  using typename Linesearch<Scalar>::FunctionSample;
+  using typename Linesearch<Scalar>::Options;
+  using fun_t = std::function<Scalar(Scalar)>;
 
-  T run(fun_t f, T phi0, T dphi0, T &a_opt);
+  Scalar run(fun_t f, Scalar phi0, Scalar dphi0, Scalar &a_opt);
   NonmonotoneLinesearch(const Options &options);
 
   void reset() {
-    mov_avg = T(0.);
-    avg_weight = T(0.);
+    mov_avg = Scalar(0.);
+    avg_weight = Scalar(0.);
   }
 
   /// Weight for moving average
-  T avg_eta = 0.85;
-  T beta_dec = 0.5;
+  Scalar avg_eta = 0.85;
+  Scalar beta_dec = 0.5;
 
 private:
-  T mov_avg;
-  T avg_weight;
+  Scalar mov_avg;
+  Scalar avg_weight;
 };
 
-template <typename T>
-NonmonotoneLinesearch<T>::NonmonotoneLinesearch(const Options &options)
-    : Linesearch<T>(options), mov_avg(0.), avg_weight(0.) {}
+template <typename Scalar>
+NonmonotoneLinesearch<Scalar>::NonmonotoneLinesearch(const Options &options)
+    : Linesearch<Scalar>(options), mov_avg(0.), avg_weight(0.) {}
 
-template <typename T>
-T NonmonotoneLinesearch<T>::run(fun_t f, T phi0, T dphi0, T &a_opt) {
+template <typename Scalar>
+Scalar NonmonotoneLinesearch<Scalar>::run(fun_t f, Scalar phi0, Scalar dphi0,
+                                          Scalar &a_opt) {
   const Options &opts = this->options_;
   mov_avg = avg_eta * avg_weight * mov_avg + phi0;
   avg_weight = avg_eta * avg_weight + 1;
@@ -45,7 +46,7 @@ T NonmonotoneLinesearch<T>::run(fun_t f, T phi0, T dphi0, T &a_opt) {
 
   while (a_opt > opts.alpha_min) {
     try {
-      const T phia = f(a_opt);
+      const Scalar phia = f(a_opt);
       bool suff_decrease = phia <= mov_avg + opts.armijo_c1 * a_opt * dphi0;
       if (suff_decrease)
         return phia;
