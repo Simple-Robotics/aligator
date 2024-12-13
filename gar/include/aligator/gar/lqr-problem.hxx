a35c2ca920787f229cd7571d85240b1117e63581
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-13 18:07:55 +0100

    Working system using the Eigen memory map system

--- gar/include/aligator/gar/lqr-problem.hxx
+++ gar/include/aligator/gar/lqr-problem.hxx
@@ -1,165 +1,152 @@
 #pragma once
 
 #include "lqr-problem.hpp"
+#include "eigen-map-management.hpp"
 
 namespace aligator::gar {
 
-namespace detail {
-template <typename EigenType, int MapOptions,
-          typename Scalar = typename EigenType::Scalar>
-void emplaceMap(Eigen::Map<EigenType, MapOptions> &map, long size,
-                Scalar *ptr) {
-  using MapType = Eigen::Map<EigenType, MapOptions>;
-  new (&map) MapType{ptr, size};
-}
-
-/// \brief Placement-new a map type using the provided memory pointer.
-template <typename EigenType, int MapOptions,
-          typename Scalar = typename EigenType::Scalar>
-void emplaceMap(Eigen::Map<EigenType, MapOptions> &map, long rows, long cols,
-                Scalar *ptr) {
-  using MapType = Eigen::Map<EigenType, MapOptions>;
-  new (&map) MapType{ptr, rows, cols};
-}
-} // namespace detail
-
 template <typename Scalar>
 LQRKnotTpl<Scalar>::LQRKnotTpl(no_alloc_t, uint nx, uint nu, uint nc, uint nx2,
-                               uint nth)
+                               uint nth, const allocator_type &alloc)
     : nx(nx), nu(nu), nc(nc), nx2(nx2), nth(nth),                          //
       Q(NULL, 0, 0), S(NULL, 0, 0), R(NULL, 0, 0), q(NULL, 0), r(NULL, 0), //
       A(NULL, 0, 0), B(NULL, 0, 0), E(NULL, 0, 0), f(NULL, 0),             //
       C(NULL, 0, 0), D(NULL, 0, 0), d(NULL, 0),                            //
       Gth(NULL, 0, 0), Gx(NULL, 0, 0), Gu(NULL, 0, 0), Gv(NULL, 0, 0),
-      gamma(NULL, 0), //
-      memory(NULL), req(Alignment) {}
-
+      gamma(NULL, 0), m_empty_after_move(true), m_allocator(alloc) {}
 template <typename Scalar>
-LQRKnotTpl<Scalar>::LQRKnotTpl(uint nx, uint nu, uint nc, uint nx2, uint nth)
-    : LQRKnotTpl(no_alloc, nx, nu, nc, nx2, nth) {
+LQRKnotTpl<Scalar>::LQRKnotTpl(uint nx, uint nu, uint nc, uint nx2, uint nth,
+                               const allocator_type &alloc)
+    : LQRKnotTpl(no_alloc, nx, nu, nc, nx2, nth, alloc) {
+  emplace_allocated_map(Q, nx, nx, m_allocator);
+  emplace_allocated_map(S, nx, nu, m_allocator);
+  emplace_allocated_map(R, nu, nu, m_allocator);
+  emplace_allocated_map(q, nx, m_allocator);
+  emplace_allocated_map(r, nu, m_allocator);
 
-  this->allocate();
-  this->initialize();
+  emplace_allocated_map(A, nx2, nx, m_allocator);
+  emplace_allocated_map(B, nx2, nu, m_allocator);
+  emplace_allocated_map(E, nx2, nx2, m_allocator);
+  emplace_allocated_map(f, nx2, m_allocator);
+
+  emplace_allocated_map(C, nc, nx, m_allocator);
+  emplace_allocated_map(D, nc, nu, m_allocator);
+  emplace_allocated_map(d, nc, m_allocator);
+
+  emplace_allocated_map(Gth, nth, nth, m_allocator);
+  emplace_allocated_map(Gx, nx, nth, m_allocator);
+  emplace_allocated_map(Gu, nu, nth, m_allocator);
+  emplace_allocated_map(Gv, nc, nth, m_allocator);
+  emplace_allocated_map(gamma, nth, m_allocator);
 }
 
-template <typename Scalar> void LQRKnotTpl<Scalar>::allocate() {
-  req.addArray<double>(nx, nx)    // Q
-      .addArray<double>(nx, nu)   // S
-      .addArray<double>(nu, nu)   // R
-      .addArray<double>(nx)       // q
-      .addArray<double>(nu)       // r
-      .addArray<double>(nx2, nx)  // A
-      .addArray<double>(nx2, nu)  // B
-      .addArray<double>(nx2, nx2) // E
-      .addArray<double>(nx2)      // f
-      .addArray<double>(nc, nx)   // C
-      .addArray<double>(nc, nu)   // D
-      .addArray<double>(nc)       // d
-      .addArray<double>(nth, nth) // Gth
-      .addArray<double>(nx, nth)  // Gx
-      .addArray<double>(nu, nth)  // Gu
-      .addArray<double>(nc, nth)  // Gv
-      .addArray<double>(nth);     // gamma
+template <typename Scalar> LQRKnotTpl<Scalar>::~LQRKnotTpl() {
+  if (!m_empty_after_move) {
+    deallocate_map(Q, m_allocator);
+    deallocate_map(S, m_allocator);
+    deallocate_map(R, m_allocator);
+    deallocate_map(q, m_allocator);
+    deallocate_map(r, m_allocator);
 
-  this->memory = static_cast<Scalar *>(req.allocate());
-  std::memset(memory, 0, req.totalBytes());
-}
+    deallocate_map(A, m_allocator);
+    deallocate_map(B, m_allocator);
+    deallocate_map(E, m_allocator);
+    deallocate_map(f, m_allocator);
 
-template <typename Scalar> void LQRKnotTpl<Scalar>::initialize() {
-  Scalar *ptr = memory;
-  detail::emplaceMap(Q, nx, nx, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(S, nx, nu, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(R, nu, nu, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(q, nx, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(r, nu, ptr);
-  req.advance(ptr);
+    deallocate_map(C, m_allocator);
+    deallocate_map(D, m_allocator);
+    deallocate_map(d, m_allocator);
 
-  detail::emplaceMap(A, nx2, nx, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(B, nx2, nu, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(E, nx2, nx2, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(f, nx2, ptr);
-  req.advance(ptr);
-
-  detail::emplaceMap(C, nc, nx, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(D, nc, nu, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(d, nc, ptr);
-  req.advance(ptr);
-
-  detail::emplaceMap(Gth, nth, nth, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(Gx, nx, nth, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(Gu, nu, nth, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(Gv, nc, nth, ptr);
-  req.advance(ptr);
-  detail::emplaceMap(gamma, nth, ptr);
-  req.advance(ptr);
-
-  req.reset();
-}
-
-template <typename Scalar>
-void LQRKnotTpl<Scalar>::addParameterization(uint nth) {
-  LQRKnotTpl copy(nx, nu, nc, nx2, nth);
-  copy.Q = Q;
-  copy.S = S;
-  copy.R = R;
-  copy.q = q;
-  copy.r = r;
-
-  copy.A = A;
-  copy.B = B;
-  copy.E = E;
-  copy.f = f;
-
-  copy.C = C;
-  copy.D = D;
-  copy.d = d;
-
-  *this = LQRKnotTpl{copy};
+    deallocate_map(Gth, m_allocator);
+    deallocate_map(Gx, m_allocator);
+    deallocate_map(Gu, m_allocator);
+    deallocate_map(Gv, m_allocator);
+    deallocate_map(gamma, m_allocator);
+  }
 }
 
 template <typename Scalar>
 LQRKnotTpl<Scalar>::LQRKnotTpl(const LQRKnotTpl &other)
-    : LQRKnotTpl(no_alloc, other.nx, other.nu, other.nc, other.nx2, other.nth) {
-  this->allocate();
-  assert(req.totalBytes() == other.req.totalBytes());
-  // copy memory over from other
-  std::memcpy(memory, other.memory, other.req.totalBytes());
-  this->initialize();
-}
+    : LQRKnotTpl(other.nx, other.nu, other.nc, other.nx2, other.nth,
+                 other.get_allocator()) {
+  this->Q = other.Q;
+  this->S = other.S;
+  this->R = other.R;
+  this->q = other.q;
+  this->r = other.r;
 
-template <typename Scalar>
-LQRKnotTpl<Scalar>::LQRKnotTpl(LQRKnotTpl &&other)
-    : LQRKnotTpl(no_alloc, other.nx, other.nu, other.nc, other.nx2, other.nth) {
-  // no need to allocate, just bring in the other
-  // memory buffer
-  memory = other.memory;
-  other.memory = NULL;
-  req = other.req;
-  this->initialize();
+  this->A = other.A;
+  this->B = other.B;
+  this->E = other.E;
+  this->f = other.f;
+
+  this->C = other.C;
+  this->D = other.D;
+  this->d = other.d;
+
+  this->Gth = other.Gth;
+  this->Gx = other.Gx;
+  this->Gu = other.Gu;
+  this->Gv = other.Gv;
+  this->gamma = other.gamma;
 }
 
 template <typename Scalar>
 LQRKnotTpl<Scalar> &LQRKnotTpl<Scalar>::operator=(const LQRKnotTpl &other) {
-  this->~LQRKnotTpl();
-  new (this) LQRKnotTpl{other};
+  LQRKnotTpl copy{other};
+  *this = std::move(copy);
   return *this;
 }
 
 template <typename Scalar>
 LQRKnotTpl<Scalar> &LQRKnotTpl<Scalar>::operator=(LQRKnotTpl &&other) {
-  swap(*this, other);
+  this->nx = other.nx;
+  this->nu = other.nu;
+  this->nc = other.nc;
+  this->nx2 = other.nx2;
+  this->nth = other.nth;
+
+  emplace_map_steal(this->Q, other.Q);
+  emplace_map_steal(this->S, other.S);
+  emplace_map_steal(this->R, other.R);
+  emplace_map_steal(this->q, other.q);
+  emplace_map_steal(this->r, other.r);
+
+  emplace_map_steal(this->A, other.A);
+  emplace_map_steal(this->B, other.B);
+  emplace_map_steal(this->E, other.E);
+  emplace_map_steal(this->f, other.f);
+
+  emplace_map_steal(this->C, other.C);
+  emplace_map_steal(this->D, other.D);
+  emplace_map_steal(this->d, other.d);
+
+  emplace_map_steal(this->Gth, other.Gth);
+  emplace_map_steal(this->Gx, other.Gx);
+  emplace_map_steal(this->Gu, other.Gu);
+  emplace_map_steal(this->Gv, other.Gv);
+  emplace_map_steal(this->gamma, other.gamma);
+
+  other.m_empty_after_move = true;
+  // reset flag if necessary
+  m_empty_after_move = false;
+  return *this;
+}
+
+template <typename Scalar>
+LQRKnotTpl<Scalar> &LQRKnotTpl<Scalar>::addParameterization(uint nth) {
+  this->nth = nth;
+  deallocate_map(Gth, m_allocator);
+  deallocate_map(Gx, m_allocator);
+  deallocate_map(Gu, m_allocator);
+  deallocate_map(Gv, m_allocator);
+  deallocate_map(gamma, m_allocator);
+
+  emplace_allocated_map(Gth, nth, nth, m_allocator);
+  emplace_allocated_map(Gx, nx, nth, m_allocator);
+  emplace_allocated_map(Gu, nx, nu, m_allocator);
+  emplace_allocated_map(Gv, nx, nc, m_allocator);
+  emplace_allocated_map(gamma, nth, m_allocator);
   return *this;
 }
 
