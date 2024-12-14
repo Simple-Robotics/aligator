e59873ec64ab6ef4ad7b036e79449586696ee4a5
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-14 13:23:01 +0100

    Add helper lqrKnotsSameDim

--- gar/include/aligator/gar/lqr-problem.hpp
+++ gar/include/aligator/gar/lqr-problem.hpp
@@ -138,6 +138,13 @@ protected:
 };
 
 template <typename Scalar>
+bool lqrKnotsSameDim(const LQRKnotTpl<Scalar> &lhs,
+                     const LQRKnotTpl<Scalar> &rhs) {
+  return (lhs.nx == rhs.nx) && (lhs.nu == rhs.nu) && (lhs.nc == rhs.nc) &&
+         (lhs.nx2 == rhs.nx2) && (lhs.nth == rhs.nth);
+}
+
+template <typename Scalar>
 std::ostream &operator<<(std::ostream &oss, const LQRKnotTpl<Scalar> &self) {
   oss << "LQRKnot {";
   oss << fmt::format("\n  nx:  {:d}", self.nx) //
