a85ab7e9bee1adf1de1bcef92621ef404b6d18e3
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-22 13:27:11 +0100

    [memory] mark allocating functions as nodiscard

--- include/aligator/memory/allocator.hpp
+++ include/aligator/memory/allocator.hpp
@@ -30,7 +30,8 @@ public:
       : std::pmr::polymorphic_allocator<byte_t>(resource) {}
 
   template <typename T>
-  T *allocate(size_t n, size_t alignment = EIGEN_DEFAULT_ALIGN_BYTES) {
+  [[nodiscard]] T *allocate(size_t n,
+                            size_t alignment = EIGEN_DEFAULT_ALIGN_BYTES) {
     return static_cast<T *>(resource()->allocate(n * sizeof(T), alignment));
   }
 
@@ -40,8 +41,9 @@ public:
     resource()->deallocate(p, n * sizeof(T), alignment);
   }
 
-  void *allocate_bytes(size_t num_bytes,
-                       size_t alignment = EIGEN_DEFAULT_ALIGN_BYTES) {
+  [[nodiscard]] void *
+  allocate_bytes(size_t num_bytes,
+                 size_t alignment = EIGEN_DEFAULT_ALIGN_BYTES) {
     return resource()->allocate(num_bytes, alignment);
   }
 
