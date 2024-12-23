19447a778e149ded007ea8b7e36d78b3ac34f306
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-23 15:39:45 +0100

    Test for precompiled headers
    - Eigen/Dense
    - pinocchio/algorithm
    - pinocchio/multibody

---
+++ include/aligator/pinocchio-algorithm.pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <pinocchio/algorithm/aba.hpp>
+#include <pinocchio/algorithm/aba-derivatives.hpp>
+
+#include <pinocchio/algorithm/center-of-mass.hpp>
+#include <pinocchio/algorithm/center-of-mass-derivatives.hpp>
+
+#include <pinocchio/algorithm/centroidal.hpp>
+#include <pinocchio/algorithm/centroidal-derivatives.hpp>
+
+#include <pinocchio/algorithm/constrained-dynamics.hpp>
+#include <pinocchio/algorithm/constrained-dynamics-derivatives.hpp>
+#include <pinocchio/algorithm/contact-jacobian.hpp>
+
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/frames-derivatives.hpp>
+
+#include <pinocchio/algorithm/joint-configuration.hpp>
+
+#include <pinocchio/algorithm/rnea.hpp>
+#include <pinocchio/algorithm/rnea-derivatives.hpp>
