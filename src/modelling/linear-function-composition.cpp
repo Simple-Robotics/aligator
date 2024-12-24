83c6f32648ee75ef24127e9b116470d5dfbae360
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-24 13:32:43 +0100

    Add template instantiation for LinearFunctionComposition

---
+++ src/modelling/linear-function-composition.cpp
@@ -0,0 +1,6 @@
+#include "aligator/modelling/linear-function-composition.hxx"
+
+namespace aligator {
+template struct LinearFunctionCompositionTpl<context::Scalar>;
+template struct LinearUnaryFunctionCompositionTpl<context::Scalar>;
+} // namespace aligator
