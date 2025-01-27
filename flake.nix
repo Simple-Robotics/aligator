94e818bd6f71e93b506f1372036a377e34d6fa36
Ludovic De Matteis <ldematteis@laas.fr>
2025-01-27 16:35:55 +0100

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Versatile and efficient framework for constrained trajectory optimization";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { pkgs, self', ... }:
+        {
+          apps.default = {
+            type = "app";
+            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
+          };
+          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          packages = {
+            default = self'.packages.aligator;
+            aligator = pkgs.python3Packages.aligator.overrideAttrs (super: {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./bench
+                  ./bindings
+                  ./CMakeLists.txt
+                  ./doc
+                  ./examples
+                  ./include
+                  ./package.xml
+                  ./src
+                  ./tests
+                  ./gar
+                ];
+              };
+            });
+          };
+        };
+    };
+}
