cec1d341f30090a6e3dfb21a297572e7385598e0
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-10 14:04:08 +0100

    flake: apply pinocchio #2541

--- flake.nix
+++ flake.nix
@@ -11,8 +11,28 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, self', ... }:
         {
+          pkgs,
+          self',
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                pinocchio = prev.pinocchio.overrideAttrs (super: {
+                  patches = (super.patches or [ ]) ++ [
+                    (final.fetchpatch {
+                      url = "https://github.com/stack-of-tasks/pinocchio/pull/2541/commits/23a638ebfb180aa7d4ea75f17e3d89477dcb6509.patch";
+                      hash = "sha256-XIZpq1JK5mY5tv3MqRk/ep6/5cJOjV2gkW1ywLjXUBU=";
+                    })
+                  ];
+                });
+              })
+            ];
+          };
           apps.default = {
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
