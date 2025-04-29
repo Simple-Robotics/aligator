92d806a59169715e7371f1e93112b813f14d3c5a
ManifoldFR <wilson.jallet@polytechnique.org>
2025-04-29 16:36:24 +0200

    [cmake] Add CMake utility aligator_create_python_extension() to generated config file

---
+++ extra-python-macros.cmake
@@ -0,0 +1,20 @@
+# ------
+# aligator_add_python_extension
+#
+#
+# ------
+function(aligator_create_python_extension name)
+  message(STATUS "Adding aligator Python C++ extension ${name}")
+  set(options WITH_SOABI)
+  cmake_parse_arguments(arg "${options}" "" "" ${ARGN})
+  set(_parse_oneValueArgs)
+  foreach(op IN LISTS options)
+    if(${arg_${op}})
+      list(APPEND _parse_oneValueArgs ${op})
+    endif()
+  endforeach()
+  set(_sources ${arg_UNPARSED_ARGUMENTS})
+
+  Python3_add_library(${name} ${_parse_oneValueArgs} ${_sources})
+  target_link_libraries(${name} PRIVATE eigenpy::eigenpy aligator::aligator)
+endfunction()
