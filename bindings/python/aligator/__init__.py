91b94e61016f5dbdc23aef0acdaf675dc648e648
ManifoldFR <wilson.jallet@polytechnique.org>
2025-05-06 16:13:05 +0200

    [python] __init__.py : do not use proxsuite_nlp's constraints

--- bindings/python/aligator/__init__.py
+++ bindings/python/aligator/__init__.py
@@ -6,8 +6,6 @@ from .pyaligator import *
 from .pyaligator import __version__
 from . import utils
 
-from proxsuite_nlp import constraints
-
 
 def _process():
     import sys
@@ -21,7 +19,6 @@ def _process():
         sys.modules[mod_name] = mod_info[1]
         mod_info[1].__file__ = pyaligator.__file__
         mod_info[1].__name__ = mod_name
-    sys.modules["{}.constraints".format(lib_name)] = constraints
 
 
 _process()
