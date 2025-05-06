99c1dabeb17dba5c4e796c75d8b0f0e13606429f
ManifoldFR <wilson.jallet@polytechnique.org>
2025-05-06 14:37:41 +0200

    constraints : add omnibus header, add tests/constraints.cpp

---
+++ include/aligator/modelling/constraints.hpp
@@ -0,0 +1,10 @@
+/// @file
+/// @copyright Copyright (C) 2023-2024 LAAS-CNRS, 2023-2025 INRIA
+/// @brief Omnibus header which includes all supported constraint sets.
+#pragma once
+
+#include "aligator/modelling/constraints/equality-constraint.hpp"
+#include "aligator/modelling/constraints/negative-orthant.hpp"
+#include "aligator/modelling/constraints/box-constraint.hpp"
+#include "aligator/modelling/constraints/l1-penalty.hpp"
+#include "aligator/modelling/constraints/constraint-set-product.hpp"
