15e21a98cc2c1c010eda1e82d86628bb533c42e3
ManifoldFR <wilson.jallet@polytechnique.org>
2025-09-17 20:15:37 +0200

    bindings/python : add plot_pd_errs to plotting utils

--- bindings/python/aligator/utils/plotting.py
+++ bindings/python/aligator/utils/plotting.py
@@ -3,6 +3,40 @@ import numpy as np
 
 from aligator import HistoryCallback, Results
 
+_ROOT_10 = 10.0**0.5
+
+
+def plot_pd_errs(ax0, prim_errs, dual_errs):
+    import matplotlib.pyplot as plt
+
+    ax0: plt.Axes
+    prim_errs = np.asarray(prim_errs)
+    dual_errs = np.asarray(dual_errs)
+    ax0.plot(prim_errs, c="tab:blue")
+    ax0.set_xlabel("Iterations")
+    col2 = "tab:orange"
+    ax0.plot(dual_errs, c=col2)
+    ax0.spines["top"].set_visible(False)
+    ax0.spines["right"].set_color(col2)
+    ax0.yaxis.label.set_color(col2)
+    ax0.set_yscale("log")
+    ax0.legend(["Primal error $p$", "Dual error $d$"])
+    ax0.set_title("Solver primal-dual residuals")
+
+    # handle scaling
+    yhigh = ax0.get_ylim()[1]
+    if len(prim_errs) == 0 or len(dual_errs) == 0:
+        return
+    mach_eps = np.finfo(float).eps
+    dmask = dual_errs > 2 * mach_eps
+    pmask = prim_errs > 2 * mach_eps
+    ymin = np.finfo(float).max
+    if dmask.any():
+        ymin = np.min(dual_errs[dmask])
+    if pmask.any() and sum(prim_errs > 0) > 0:
+        ymin = min(np.min(prim_errs[pmask]), ymin)
+    ax0.set_ylim(ymin / _ROOT_10, yhigh)
+
 
 def plot_convergence(
     cb: HistoryCallback,
@@ -12,8 +46,6 @@ def plot_convergence(
     show_al_iters=False,
     legend_kwargs={},
 ):
-    from proxsuite_nlp.utils import plot_pd_errs
-
     prim_infeas = cb.prim_infeas.tolist()
     dual_infeas = cb.dual_infeas.tolist()
     if res is not None:
