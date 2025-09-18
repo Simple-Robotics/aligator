59f65a5ffef55d932a2e9663bdfbbe28c474b8c9
ManifoldFR <wilson.jallet@polytechnique.org>
2025-09-18 15:06:42 +0200

    core/linesearches : move LinesearchOptions struct out of the Linesearch template class
    + turn on CTAD

--- include/aligator/core/linesearch-base.hpp
+++ include/aligator/core/linesearch-base.hpp
@@ -10,41 +10,45 @@ namespace aligator {
 enum class LinesearchStrategy { ARMIJO, WOLFE };
 enum class LSInterpolation { BISECTION, QUADRATIC, CUBIC };
 
+template <typename T> struct LinesearchOptions {
+  LinesearchOptions() noexcept
+      : armijo_c1(1e-4)
+      , wolfe_c2(0.9)
+      , dphi_thresh(1e-13)
+      , alpha_min(1e-6)
+      , max_num_steps(20)
+      , interp_type(LSInterpolation::CUBIC)
+      , contraction_min(0.5)
+      , contraction_max(0.8) {}
+  T armijo_c1;
+  T wolfe_c2;
+  T dphi_thresh;
+  T alpha_min;
+  std::size_t max_num_steps;
+  LSInterpolation interp_type;
+  T contraction_min;
+  T contraction_max;
+  friend std::ostream &operator<<(std::ostream &oss,
+                                  const LinesearchOptions &self) {
+    oss << "{";
+    oss << fmt::format("armijo_c1 = {:.3e}", self.armijo_c1);
+    oss << ", "
+        << fmt::format("contraction_min = {:.3e}", self.contraction_min);
+    oss << ", "
+        << fmt::format("contraction_max = {:.3e}", self.contraction_max);
+    oss << "}";
+    return oss;
+  }
+};
+
 /// @brief Base linesearch class.
 /// Design pattern inspired by Google Ceres-Solver.
 template <typename T> class Linesearch {
 public:
-  struct Options {
-    Options()
-        : armijo_c1(1e-4)
-        , wolfe_c2(0.9)
-        , dphi_thresh(1e-13)
-        , alpha_min(1e-6)
-        , max_num_steps(20)
-        , interp_type(LSInterpolation::CUBIC)
-        , contraction_min(0.5)
-        , contraction_max(0.8) {}
-    T armijo_c1;
-    T wolfe_c2;
-    T dphi_thresh;
-    T alpha_min;
-    std::size_t max_num_steps;
-    LSInterpolation interp_type;
-    T contraction_min;
-    T contraction_max;
-    friend std::ostream &operator<<(std::ostream &oss, const Options &self) {
-      oss << "{";
-      oss << fmt::format("armijo_c1 = {:.3e}", self.armijo_c1);
-      oss << ", "
-          << fmt::format("contraction_min = {:.3e}", self.contraction_min);
-      oss << ", "
-          << fmt::format("contraction_max = {:.3e}", self.contraction_max);
-      oss << "}";
-      return oss;
-    }
-  };
-  explicit Linesearch(const Linesearch::Options &options);
-  ~Linesearch();
+  using Options = LinesearchOptions<T>;
+  explicit Linesearch(const Linesearch::Options &options) noexcept
+      : options_(options) {}
+  ~Linesearch() = default;
 
   struct FunctionSample {
     T alpha;
@@ -74,11 +78,4 @@ public:
 
   Linesearch::Options options_;
 };
-
-template <typename T>
-Linesearch<T>::Linesearch(const Linesearch::Options &options)
-    : options_(options) {}
-
-template <typename T> Linesearch<T>::~Linesearch() = default;
-
 } // namespace aligator
