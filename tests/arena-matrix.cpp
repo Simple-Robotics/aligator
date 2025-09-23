c91969aada013d9bb0bbfafa86678beb96fa3123
ManifoldFR <wilson.jallet@polytechnique.org>
2025-09-23 18:28:32 +0200

    tests : use Catch2 for arena-matrix.cpp

--- tests/arena-matrix.cpp
+++ tests/arena-matrix.cpp
@@ -1,4 +1,5 @@
-#include <boost/test/unit_test.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
 
 #include "aligator/memory/arena-matrix.hpp"
 
@@ -20,36 +21,34 @@ static_assert(!alloc_traits::propagate_on_container_swap());
 std::pmr::monotonic_buffer_resource g_res{40000};
 polymorphic_allocator g_alloc{&g_res};
 
-BOOST_AUTO_TEST_SUITE(managed_matrix)
-
-BOOST_AUTO_TEST_CASE(managed_matrix_create_copy) {
+TEST_CASE("managed_matrix_create_copy") {
   AllocMatrixType a{10, 10, g_alloc};
-  BOOST_CHECK(a.get_allocator() == g_alloc);
-  BOOST_CHECK(a.size() == 100);
-  BOOST_CHECK(a.allocatedSize() == a.size());
+  REQUIRE(a.get_allocator() == g_alloc);
+  REQUIRE(a.size() == 100);
+  REQUIRE(a.allocatedSize() == a.size());
 
   AllocMatrixType b{a};
   // copy ctor does not propagate allocator
-  BOOST_CHECK(a.get_allocator() != b.get_allocator());
-  BOOST_CHECK(a.data() != b.data());
+  REQUIRE(a.get_allocator() != b.get_allocator());
+  REQUIRE(a.data() != b.data());
 
   // copy ctor using original allocator
   AllocMatrixType c{a, g_alloc};
-  BOOST_CHECK(a.get_allocator() == c.get_allocator());
-  BOOST_CHECK(a.data() != c.data());
+  REQUIRE(a.get_allocator() == c.get_allocator());
+  REQUIRE(a.data() != c.data());
 }
 
-BOOST_AUTO_TEST_CASE(managed_matrix_copy_asignment) {
+TEST_CASE("managed_matrix_copy_asignment") {
   AllocMatrixType a{10, 10, g_alloc};
   a.setOnes();
   AllocMatrixType d = a;
-  BOOST_CHECK(a.get_allocator() != d.get_allocator());
-  BOOST_CHECK(a.isApprox(d));
+  REQUIRE(a.get_allocator() != d.get_allocator());
+  REQUIRE(a.isApprox(d));
 
   AllocMatrixType c{g_alloc};
-  BOOST_CHECK(a.get_allocator() == c.get_allocator());
+  REQUIRE(a.get_allocator() == c.get_allocator());
   c = a;
-  BOOST_CHECK(a.isApprox(c));
+  REQUIRE(a.isApprox(c));
 
   std::cout << "c matrix:\n" << c << std::endl;
   c.setIdentity();
@@ -58,7 +57,7 @@ BOOST_AUTO_TEST_CASE(managed_matrix_copy_asignment) {
   std::cout << "c = Id + a matrix:\n" << c << std::endl;
 
   c = a + a;
-  BOOST_CHECK(c.isApprox(a + a));
+  REQUIRE(c.isApprox(a + a));
   std::cout << "c = a + a matrix:\n" << c << std::endl;
   c.noalias() = a * a;
   std::cout << "c = a * a matrix:\n" << c << std::endl;
@@ -67,7 +66,7 @@ BOOST_AUTO_TEST_CASE(managed_matrix_copy_asignment) {
   MatrixXd b;
   b.setRandom(10, 10);
   c.noalias() = a * b;
-  BOOST_CHECK(c.isApprox(a * b));
+  REQUIRE(c.isApprox(a * b));
 
   c.topRows(4).setZero();
   std::cout << "c (top 4 is 0):\n" << c << std::endl;
@@ -76,18 +75,16 @@ BOOST_AUTO_TEST_CASE(managed_matrix_copy_asignment) {
   std::cout << "a.setZero(4, 4):\n" << a << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(managed_matrix_resize) {
+TEST_CASE("managed_matrix_resize") {
   AllocMatrixType a{4, 6, g_alloc};
-  BOOST_CHECK_EQUAL(a.rows(), 4);
-  BOOST_CHECK_EQUAL(a.cols(), 6);
+  REQUIRE(a.rows() == 4);
+  REQUIRE(a.cols() == 6);
 
-  BOOST_TEST_MESSAGE("resize");
   a.resize(4, 8);
-  BOOST_CHECK_EQUAL(a.rows(), 4);
-  BOOST_CHECK_EQUAL(a.cols(), 8);
+  REQUIRE(a.rows() == 4);
+  REQUIRE(a.cols() == 8);
   std::cout << "a (4, 8) (uninitialized):\n" << a << std::endl;
 
-  BOOST_TEST_MESSAGE("conservativeResize");
   a.conservativeResize(4, 3);
   a.setConstant(3.14);
   std::cout << "a.conservativeResize(4, 4):\n" << a << std::endl;
@@ -100,28 +97,24 @@ BOOST_AUTO_TEST_CASE(managed_matrix_resize) {
             << a << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(managed_matrix_move) {
+TEST_CASE("managed_matrix_move") {
   AllocMatrixType a{6, 10, g_alloc};
   auto *pdata = a.data();
   a.setRandom();
-  BOOST_CHECK_EQUAL(a.rows(), 6);
-  BOOST_CHECK_EQUAL(a.cols(), 10);
-  BOOST_CHECK(a.get_allocator() == g_alloc);
+  REQUIRE(a.rows() == 6);
+  REQUIRE(a.cols() == 10);
+  REQUIRE(a.get_allocator() == g_alloc);
 
   AllocMatrixType a_copy = a;
 
-  BOOST_TEST_MESSAGE("Move ctor");
   AllocMatrixType b{std::move(a)};
-  BOOST_CHECK(b.get_allocator() == g_alloc);
-  BOOST_CHECK(b.data() == pdata);
-  BOOST_CHECK(b == a_copy);
+  REQUIRE(b.get_allocator() == g_alloc);
+  REQUIRE(b.data() == pdata);
+  REQUIRE(b == a_copy);
 
-  BOOST_TEST_MESSAGE("Move assignment op");
   AllocMatrixType c;
   c = std::move(b);
-  BOOST_CHECK(c.get_allocator() == polymorphic_allocator{});
-  BOOST_CHECK(c.data() != pdata);
-  BOOST_CHECK(c == a_copy);
+  REQUIRE(c.get_allocator() == polymorphic_allocator{});
+  REQUIRE(c.data() != pdata);
+  REQUIRE(c == a_copy);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
