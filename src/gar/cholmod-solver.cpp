acf8a77e3963d2216accc71990517f31aa78ae82
ManifoldFR <wilson.jallet@polytechnique.org>
2025-09-29 14:33:33 +0200

    [gar] Remove CHOLMOD backend for linear solver
    update pixi lockfile

--- src/gar/cholmod-solver.cpp
+++
@@ -1,8 +0,0 @@
-/// @copyright Copyright (C) 2024 LAAS-CNRS, INRIA
-#include "aligator/gar/cholmod-solver.hxx"
-
-#ifdef ALIGATOR_WITH_CHOLMOD
-namespace aligator::gar {
-template class CholmodLqSolver<context::Scalar>;
-} // namespace aligator::gar
-#endif
