6002d952c045de23147d1f09abc368a970a71b8f
ManifoldFR <wilson.jallet@polytechnique.org>
2025-10-09 17:12:38 +0200

    tests/python : rework @edantec 's test using Pytest fixtures

--- tests/python/test_mpc.py
+++ tests/python/test_mpc.py
@@ -7,57 +7,55 @@ import numpy as np
 import aligator
 import pytest
 
-rmodel = pin.buildSampleModelHumanoid()
-rdata: pin.Data = rmodel.createData()
-space = aligator.manifolds.MultibodyPhaseSpace(rmodel)
-gravity = np.array([0, 0, -9.81])
 
-dt = 1e-2  # Timestep
-mu = 0.8  # Friction coefficient
-mass = pin.computeTotalMass(rmodel)
-f_ref = np.array([0, 0, -mass * gravity[2] / 4.0])  # Initial contact force
-
-# Contact models
-FOOT_FRAME_IDS = {
-    fname: rmodel.getFrameId(fname)
-    for fname in ["lleg_effector_body", "rleg_effector_body"]
-}
-FOOT_JOINT_IDS = {
-    fname: rmodel.frames[fid].parentJoint for fname, fid in FOOT_FRAME_IDS.items()
-}
-
-nq = rmodel.nq
-nv = rmodel.nv
-nu = rmodel.nv
-act_matrix = np.eye(nv, nu, -6)
-prox_settings = pin.ProximalSettings(1e-9, 1e-10, 10)
-constraint_models = []
-constraint_datas = []
-for fname, fid in FOOT_FRAME_IDS.items():
-    joint_id = FOOT_JOINT_IDS[fname]
-    pl1 = rmodel.frames[fid].placement
-    pl2 = rdata.oMf[fid]
-    cm = pin.RigidConstraintModel(
-        pin.ContactType.CONTACT_3D,
-        rmodel,
-        joint_id,
-        pl1,
-        0,
-        pl2,
-        pin.LOCAL,
-    )
-    cm.corrector.Kp[:] = (0, 0, 0)
-    cm.corrector.Kd[:] = (0, 0, 0)
-    constraint_models.append(cm)
-    constraint_datas.append(cm.createData())
-
-q0 = space.neutral()
-x0 = np.concatenate((q0, np.zeros(nv)))
-u0 = np.zeros(nu)
+NUM_MPC_CYCLES = 100
 
 
-@pytest.mark.skip("This is known to fail, and must be fixed.")
-def test_parallel_mpc():
+@pytest.fixture
+def mpc_problem():
+    rmodel = pin.buildSampleModelHumanoid()
+    rdata: pin.Data = rmodel.createData()
+    space = aligator.manifolds.MultibodyPhaseSpace(rmodel)
+
+    dt = 1e-2  # Timestep
+
+    # Contact models
+    FOOT_FRAME_IDS = {
+        fname: rmodel.getFrameId(fname)
+        for fname in ["lleg_effector_body", "rleg_effector_body"]
+    }
+    FOOT_JOINT_IDS = {
+        fname: rmodel.frames[fid].parentJoint for fname, fid in FOOT_FRAME_IDS.items()
+    }
+
+    nv = rmodel.nv
+    nu = rmodel.nv
+    act_matrix = np.eye(nv, nu, -6)
+    prox_settings = pin.ProximalSettings(1e-9, 1e-10, 10)
+    constraint_models = []
+    constraint_datas = []
+    for fname, fid in FOOT_FRAME_IDS.items():
+        joint_id = FOOT_JOINT_IDS[fname]
+        pl1 = rmodel.frames[fid].placement
+        pl2 = rdata.oMf[fid]
+        cm = pin.RigidConstraintModel(
+            pin.ContactType.CONTACT_3D,
+            rmodel,
+            joint_id,
+            pl1,
+            0,
+            pl2,
+            pin.LOCAL,
+        )
+        cm.corrector.Kp[:] = (0, 0, 0)
+        cm.corrector.Kd[:] = (0, 0, 0)
+        constraint_models.append(cm)
+        constraint_datas.append(cm.createData())
+
+    q0 = space.neutral()
+    x0 = np.concatenate((q0, np.zeros(nv)))
+    u0 = np.zeros(nu)
+
     T = 50
 
     stages = []
@@ -75,6 +73,11 @@ def test_parallel_mpc():
 
     term_cost = aligator.CostStack(space, nu)
     problem = aligator.TrajOptProblem(x0, stages, term_cost)
+    return problem
+
+
+def test_parallel_mpc(mpc_problem):
+    problem = mpc_problem
 
     TOL = 1e-5
     mu_init = 1e-8
@@ -95,7 +98,7 @@ def test_parallel_mpc():
     us = solver.results.us.tolist().copy()
 
     # Launch MPC
-    for t in range(100):
+    for t in range(NUM_MPC_CYCLES):
         print("Time " + str(t))
 
         xs = xs[1:] + [xs[-1]]
@@ -109,24 +112,8 @@ def test_parallel_mpc():
         us = solver.results.us.tolist().copy()
 
 
-def test_serial_mpc():
-    T = 50
-
-    stages = []
-    for _ in range(T):
-        rcost = aligator.CostStack(space, nu)
-        rcost.addCost(aligator.QuadraticStateCost(space, nu, x0, np.eye(space.ndx)))
-        rcost.addCost(aligator.QuadraticControlCost(space, u0, np.eye(nu) * 1e-4))
-
-        ode = aligator.dynamics.MultibodyConstraintFwdDynamics(
-            space, act_matrix, constraint_models, prox_settings
-        )
-        dyn_model = aligator.dynamics.IntegratorSemiImplEuler(ode, dt)
-        stm = aligator.StageModel(rcost, dyn_model)
-        stages.append(stm)
-
-    term_cost = aligator.CostStack(space, nu)
-    problem = aligator.TrajOptProblem(x0, stages, term_cost)
+def test_serial_mpc(mpc_problem):
+    problem = mpc_problem
 
     TOL = 1e-5
     mu_init = 1e-8
@@ -146,7 +133,7 @@ def test_serial_mpc():
     us = solver.results.us.tolist().copy()
 
     # Launch MPC
-    for t in range(100):
+    for t in range(NUM_MPC_CYCLES):
         print("Time " + str(t))
 
         xs = xs[1:] + [xs[-1]]
