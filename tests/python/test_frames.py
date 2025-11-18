d3ef370e39c0aeeb982f75af336da5ccf4211bbd
earlaud <etienne.arlaud@inria.fr>
2025-11-18 11:08:06 +0100

    Test frame equality ref placement

--- tests/python/test_frames.py
+++ tests/python/test_frames.py
@@ -47,22 +47,25 @@ def test_frame_equality_finite_differences():
     assert fr_id1 == two_frame_residual.frame1_id
     assert fr_id2 == two_frame_residual.frame2_id
 
-    finite_differences = aligator.FiniteDifferenceHelper(
-        space, two_frame_residual, FD_EPS
-    )
-    finite_differences_data = finite_differences.createData()
-
     x = np.zeros(space.nx)
     for _ in range(10):
-        x[3:] = space.rand()[3:]
-        u0 = np.zeros(nu)
-        two_frame_residual.evaluate(x, two_frame_residual_data)
-        two_frame_residual.computeJacobians(x, two_frame_residual_data)
-        finite_differences.evaluate(x, u0, finite_differences_data)
-        finite_differences.computeJacobians(x, u0, finite_differences_data)
-        assert np.allclose(
-            two_frame_residual_data.Jx, finite_differences_data.Jx, atol=ATOL
+        reference_transform = pin.SE3.Random()
+        two_frame_residual.setReference(reference_transform)
+        assert reference_transform == two_frame_residual.getReference()
+        finite_differences = aligator.FiniteDifferenceHelper(
+            space, two_frame_residual, FD_EPS
         )
+        finite_differences_data = finite_differences.createData()
+        for _ in range(10):
+            x[3:] = space.rand()[3:]
+            u0 = np.zeros(nu)
+            two_frame_residual.evaluate(x, two_frame_residual_data)
+            two_frame_residual.computeJacobians(x, two_frame_residual_data)
+            finite_differences.evaluate(x, u0, finite_differences_data)
+            finite_differences.computeJacobians(x, u0, finite_differences_data)
+            assert np.allclose(
+                two_frame_residual_data.Jx, finite_differences_data.Jx, atol=ATOL
+            )
 
 
 def test_frame_equality_against_frame_placement():
@@ -88,8 +91,16 @@ def test_frame_equality_against_frame_placement():
 
     x = np.zeros(space.nx)
     for _ in range(10):
+        # Set a random reference pose
+        reference_transform = pin.SE3.Random()
+        frame_placement_residual.setReference(reference_transform)
+        two_frame_residual.setReference(reference_transform)
+        assert reference_transform == two_frame_residual.getReference()
+
+        # Take a random state
         x[3:] = space.rand()[3:]
         u0 = np.zeros(nu)
+
         two_frame_residual.evaluate(x, two_frame_residual_data)
         two_frame_residual.computeJacobians(x, two_frame_residual_data)
         frame_placement_residual.evaluate(x, u0, frame_placement_residual_data)
