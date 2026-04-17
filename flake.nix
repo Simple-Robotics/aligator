85218c54ba81680874189ddaaec8d66769095722
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-17 16:23:44 +0200

    nix: switch to flakoboros

--- flake.nix
+++ flake.nix
@@ -1,78 +1,30 @@
 {
   description = "Versatile and efficient framework for constrained trajectory optimization";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          apps.default = {
-            type = "app";
-            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-          };
-          packages = {
-            default = self'.packages.py-aligator;
-            aligator = pkgs.aligator.overrideAttrs (super: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./bench
-                  ./bindings
-                  ./CMakeLists.txt
-                  ./extra-python-macros.cmake
-                  ./doc
-                  ./examples
-                  ./include
-                  ./package.xml
-                  ./src
-                  ./tests
-                ];
-              };
-              buildInputs = (super.buildInputs or []) ++ [
-                pkgs.mimalloc
-              ];
-              checkInputs = super.checkInputs ++ [
-                pkgs.catch2_3
-              ];
-            });
-            py-aligator = pkgs.python3Packages.toPythonModule (self'.packages.aligator.overrideAttrs (super:{
-              cmakeFlags = super.cmakeFlags ++ [
-                (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
-                (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" true)
-              ];
-              nativeBuildInputs = super.nativeBuildInputs ++ [
-                pkgs.python3Packages.python
-              ];
-              propagatedBuildInputs = super.propagatedBuildInputs ++ [
-                self'.packages.aligator
-                pkgs.python3Packages.crocoddyl
-                pkgs.python3Packages.pinocchio
-              ];
-              nativeCheckInputs = [
-                pkgs.ctestCheckHook
-                pkgs.python3Packages.pythonImportsCheckHook
-              ];
-              checkInputs = super.checkInputs++ [
-                pkgs.python3Packages.matplotlib
-                pkgs.python3Packages.pytest
-              ];
-              disabledTests = lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-                "aligator-test-py-rollout"
-              ];
-            }));
+    inputs.gepetto.lib.mkFlakoboros inputs (
+      { lib, ... }:
+      {
+        overrideAttrs.aligator = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./bench
+              ./bindings
+              ./CMakeLists.txt
+              ./extra-python-macros.cmake
+              ./doc
+              ./examples
+              ./include
+              ./package.xml
+              ./src
+              ./tests
+            ];
           };
         };
-    };
+      }
+    );
 }
